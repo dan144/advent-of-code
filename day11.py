d53d751dc4b3c245f923117692a217cdca9a6f5c
Daniel Gross <dan.12sq@gmail.com>
2018-12-01 04:38:35 +0000

    improve framework

--- day11.py
+++ day11.py
@@ -3,3 +3,13 @@
 import sys
 
 print('Running:',sys.argv[0])
+
+n = sys.argv[0][2:].index('.')
+filen = sys.argv[0][5:n+1+len(str(n))]
+input_file = 'input' + str(filen)
+print('Reading:',input_file)
+
+
+with open(input_file, 'r') as f:
+    for line in f:
+        print(line)
