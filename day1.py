dd963542ea635af43ca4eee95f62eeeb29caed23
Daniel Gross <dan.12sq@gmail.com>
2018-12-02 15:11:53 +0000

    refactored day 1 to use sets, readd part 1 (smarter)

--- day1.py
+++ day1.py
@@ -9,25 +9,31 @@ filen = sys.argv[0][5:n+1+len(str(n))]
 input_file = 'input' + str(filen)
 print('Reading:',input_file)
 
+diffs = []
+with open(input_file, 'r') as f:
+    for line in f:
+        diffs.append(int(line[:-1]))
+
+print()
+print('PART ONE')
+print('Sum:', sum(diffs))
+
+print()
+print('PART TWO')
+
 total = 0
-totals = []
+totals = None
 ans = None
 while not ans:
-    if totals == []:
-        totals = [0]
-    with open(input_file, 'r') as f:
-        for line in f:
-            if line[0] == "+":
-                total += int(line[1:])
-            elif line[0] == '-':
-                total -= int(line[1:])
-            else:
-                continue
-            #print(line)
-            if total in totals:
-                ans = total
-                break
-            else:
-                totals.append(total)
+    if totals == None:
+        totals = {0}
+    for line in diffs:
+        total += line
+        #print(line)
+        if total in totals:
+            ans = total
+            break
+        else:
+            totals.add(total)
     #print(len(totals), totals[-1])
-print(ans)
+print('First repeated:', ans)
