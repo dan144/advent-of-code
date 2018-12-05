46d3fcd611bbffabecee12ce67acf9e2a8314fea
Daniel Gross <dan.12sq@gmail.com>
2018-12-05 13:18:58 +0000

    refactor day 5 - create new list instead of reusing full list

--- day5.py
+++ day5.py
@@ -19,27 +19,17 @@ with open(input_file, 'r') as f:
 print()
 print('PART ONE')
 
-val = list(inputs[0])
-print('Original polymer length:', len(val))
-removed = True
-while removed:
-    removed = False
-    try:
-        for i in range(len(val) - 1):
-            if val[i].isupper() and val[i+1].islower():
-                if val[i].lower() == val[i+1]:
-                    val.pop(i)
-                    val.pop(i)
-                    i -= 1
-                    removed = True
-            if val[i].islower() and val[i+1].isupper():
-                if val[i].upper() == val[i+1]:
-                    val.pop(i)
-                    val.pop(i)
-                    i -= 1
-                    removed = True
-    except IndexError:
-        continue
+val = []
+print('Original polymer length:', len(inputs[0]))
+for c in inputs[0]:
+    if len(val) > 0:
+        if c.isupper() and val[-1].islower() and c.lower() == val[-1]:
+            val.pop()
+            continue
+        if c.islower() and val[-1].isupper() and c.upper() == val[-1]:
+            val.pop()
+            continue
+    val.append(c)
 print('Reacted polymer length: ', len(val))
 
 print()
@@ -49,29 +39,17 @@ min_polymer_len = None
 min_letter = None
 for letter in string.ascii_lowercase:
     val = []
-    for c in list(inputs[0]):
-        if c.lower() != letter:
-            val.append(c)
-    removed = True
-    while removed:
-        removed = False
-        try:
-            for i in range(len(val) - 1):
-                if val[i].isupper() and val[i+1].islower():
-                    if val[i].lower() == val[i+1]:
-                        val.pop(i)
-                        val.pop(i)
-                        i -= 1
-                        removed = True
-                if val[i].islower() and val[i+1].isupper():
-                    if val[i].upper() == val[i+1]:
-                        val.pop(i)
-                        val.pop(i)
-                        i -= 1
-                        removed = True
-        except IndexError:
+    for c in inputs[0]:
+        if c.lower() == letter:
             continue
-    print('Removing', letter, 'gives', len(val))
+        if len(val) > 0:
+            if c.isupper() and val[-1].islower() and c.lower() == val[-1]:
+                val.pop()
+                continue
+            if c.islower() and val[-1].isupper() and c.upper() == val[-1]:
+                val.pop()
+                continue
+        val.append(c)
     if min_polymer_len is None or len(val) < min_polymer_len:
         min_letter = letter
         min_polymer_len = len(val)
