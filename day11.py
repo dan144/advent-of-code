d3cc69cf01d5be2e553563a06b7b8bb656c5c92b
Daniel Gross <dan.12sq@gmail.com>
2018-12-08 03:21:30 +0000

    add framework: complete testing infrastructure

--- day11.py
+++ day11.py
@@ -1,18 +1,57 @@
 #!/usr/bin/python3
 
+import json
 import sys
 
 print('Running:',sys.argv[0])
 
+testing = len(sys.argv) == 2
+
 n = sys.argv[0][2:].index('.')
 filen = sys.argv[0][5:n+1+len(str(n))]
 input_file = 'input' + str(filen)
+
+if testing:
+    print('TESTING')
+    input_file = 'test' + input_file
+
 print('Reading:', input_file)
 
 inputs = []
 data_type = str
 with open(input_file, 'r') as f:
+    if testing:
+        test_vals = json.loads(f.readline())
+        part_one = test_vals['part_one']
+        part_two = test_vals['part_two']
     for line in f:
         inputs.append(data_type(line[:-1]))
+if testing:
+    print(inputs)
 
-print(inputs)
+print()
+print('PART ONE')
+ans = None
+
+
+
+print(ans)
+if testing:
+    if part_one == ans:
+        print('PART ONE CORRECT')
+    else:
+        print('PART ONE FAILED')
+
+
+print()
+print('PART TWO')
+ans = None
+
+
+
+print(ans)
+if testing:
+    if part_two == ans:
+        print('PART TWO CORRECT')
+    else:
+        print('PART TWO FAILED')
