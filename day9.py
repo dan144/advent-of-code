65cbbdf4a87735d9fc6d55dd4ae245b5a0389853
Daniel Gross <dan.12sq@gmail.com>
2018-12-09 07:28:18 +0000

    day 9 solution

--- day9.py
+++ day9.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+import collections
 import json
 import sys
 
@@ -26,14 +27,53 @@ with open(input_file, 'r') as f:
         part_two = test_vals['part_two']
     for line in f:
         inputs.append(data_type(line[:-1]))
+players = int(inputs[0].split()[0])
+marbles = int(inputs[0].split()[6])
 if testing:
-    print(inputs)
+    print(players, marbles)
 
 print()
 print('PART ONE')
-ans = None
+ans = 0
 
-
+circle = collections.deque()
+scores = [0] * players
+player = -1
+current = 0
+for m in range(marbles + 1):
+    if m % 10000 == 0:
+        print(m)
+    player = (player + 1) % players
+    if m > 0 and m % 23 == 0:
+        scores[player] += m
+        remove = current - 7
+        if remove > 0:
+            circle.rotate(remove * -1)
+            scores[player] += circle.popleft()
+            current = 0
+        elif remove < 0:
+            circle.rotate(remove)
+            scores[player] += circle.pop()
+            current = 0
+        else:
+            scores[player] += circle.pop()
+            current = 0
+        #m = circle[new_current]
+    else:
+        if len(circle) < 2 or current + 2 == len(circle):
+            circle.append(m)
+            current = len(circle) - 1
+        else:
+            offset = 2
+            current = (current + offset) % (len(circle))
+            circle.rotate(current * -1)
+            circle.appendleft(m)
+            circle.rotate(current)
+        #current = circle.index(m)
+    if testing and len(circle) < 50:
+        print(player, current, circle[current], circle)
+#print(scores)
+ans = max(scores)
 
 print(ans)
 if testing:
@@ -45,7 +85,7 @@ if testing:
 
 print()
 print('PART TWO')
-ans = None
+ans = 0
 
 
 
