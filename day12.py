250619193e786cae768df2ff567e4727adf5a0ca
Daniel Gross <dan.12sq@gmail.com>
2018-12-12 06:25:38 +0000

    day 12 solution

--- day12.py
+++ day12.py
@@ -33,21 +33,50 @@ print()
 print('PART ONE')
 ans = None
 
+# initial state: ##.#...#.#.#....###.#.#....##.#...##.##.###..#.##.###..####.#..##..#.##..#.......####.#.#..#....##.#
+# #.#.# => #
+board = list(inputs[0].split(': ')[1])
+actions = {}
+for a in inputs[2:]:
+    i, o = a.split(' => ', 1)
+    actions[i] = o
 
+m = 0
+generations = 90
+for i in range(generations):
+    board = ['.']*4 + board + ['.']*4
+    m += 2
+    if testing:
+        print(i, '\t', ''.join(board))
+    old = ''.join(board[:5])
+    board = board[5:]
+    n = [actions.get(old, '.')]
+    while board:
+        old = old[1:] + board.pop(0)
+        n.append(actions.get(old, '.'))
+    while n[0] == '.':
+        m -= 1
+        n.pop(0)
+    while n[-1] == '.':
+        n.pop()
+    board = n
+    if i == 20:
+        ans = sum([i - m for i in range(len(board)) if board[i] == '#'])
+        print(ans)
 
-print(ans)
 if testing:
     if part_one == ans:
         print('PART ONE CORRECT')
     else:
         print('PART ONE FAILED')
 
-
 print()
 print('PART TWO')
-ans = None
 
+# pattern:  at generation 89, the number of plants is 5145
+#           after that, each generation increases by 50
 
+ans = sum([i - m for i in range(len(board)) if board[i] == '#']) + (50000000000 - generations) * 50
 
 print(ans)
 if testing:
