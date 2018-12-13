d2e491dd4ef5fa2044e088d129078b0620c7aa0c
Daniel Gross <dan.12sq@gmail.com>
2018-12-13 14:49:33 +0000

    day 13 solution

--- day13.py
+++ day13.py
@@ -29,12 +29,93 @@ with open(input_file, 'r') as f:
 if testing:
     print(inputs)
 
-print()
-print('PART ONE')
+board = []
+carts = []
+tick = 0
+ncarts = 0
+
+y = 0
+for line in inputs:
+    board.append(list(line))
+    carts.append([None] * len(line))
+    x = 0
+    for c in line:
+        if c in '<>^v':
+            ncarts += 1
+            carts[y][x] = [c, 0, tick]
+            if c in '<>':
+                board[y][x] = '-'
+            else:
+                board[y][x] = '|'
+        x += 1
+    y += 1
+
+my = len(board)
+mx = len(board[0])
+crash = None
 ans = None
 
+print('Total carts:', ncarts)
 
+print()
+print('PART ONE')
 
+while ncarts > 1:
+    for y in range(my):
+        for x in range(mx):
+            if carts[y][x] is None or carts[y][x][2] > tick:
+                continue
+            m = carts[y][x][0] + board[y][x]
+            if m in {'<-', '^\\', 'v/'}:
+                ny, nx, c, d = y, x-1, '<', carts[y][x][1]
+            elif m in {'>-', '^/', 'v\\'}:
+                ny, nx, c, d = y, x+1, '>', carts[y][x][1]
+            elif m in {'v|', '</', '>\\'}:
+                ny, nx, c, d = y+1, x, 'v', carts[y][x][1]
+            elif m in {'^|', '<\\', '>/'}:
+                ny, nx, c, d = y-1, x, '^', carts[y][x][1]
+            elif board[y][x] == '+':
+                if carts[y][x][1] == 0:  # left
+                    if carts[y][x][0] == '<':
+                        ny, nx, c, d = y+1, x, 'v', 1
+                    elif carts[y][x][0] == 'v':
+                        ny, nx, c, d = y, x+1, '>', 1
+                    elif carts[y][x][0] == '>':
+                        ny, nx, c, d = y-1, x, '^', 1
+                    elif carts[y][x][0] == '^':
+                        ny, nx, c, d = y, x-1, '<', 1
+                if carts[y][x][1] == 1:  # straight
+                    if carts[y][x][0] == '<':
+                        ny, nx, c, d = y, x-1, '<', 2
+                    elif carts[y][x][0] == 'v':
+                        ny, nx, c, d = y+1, x, 'v', 2
+                    elif carts[y][x][0] == '>':
+                        ny, nx, c, d = y, x+1, '>', 2
+                    elif carts[y][x][0] == '^':
+                        ny, nx, c, d = y-1, x, '^', 2
+                if carts[y][x][1] == 2:  # right
+                    if carts[y][x][0] == '<':
+                        ny, nx, c, d = y-1, x, '^', 0
+                    elif carts[y][x][0] == 'v':
+                        ny, nx, c, d = y, x-1, '<', 0
+                    elif carts[y][x][0] == '>':
+                        ny, nx, c, d = y+1, x, 'v', 0
+                    elif carts[y][x][0] == '^':
+                        ny, nx, c, d = y, x+1, '>', 0
+            if carts[ny][nx] is not None:
+                if crash is None:
+                    crash = ','.join((str(nx), str(ny)))
+                    print(crash)
+                carts[y][x] = None
+                carts[ny][nx] = None
+                ncarts -= 2
+                print('Remaining carts:', ncarts)
+            else:
+                carts[ny][nx] = [c, d, tick+1]
+                carts[y][x] = None
+    tick += 1
+
+ans = crash
 print(ans)
 if testing:
     if part_one == ans:
@@ -42,12 +123,15 @@ if testing:
     else:
         print('PART ONE FAILED')
 
-
 print()
 print('PART TWO')
 ans = None
 
-
+for y in range(len(carts)):
+    for x in range(len(carts[0])):
+        if carts[y][x]:
+            ans = ','.join((str(x), str(y)))
+            break
 
 print(ans)
 if testing:
