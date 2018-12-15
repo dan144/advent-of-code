14060f430f8d2e46ad36a58476fe556897e1dc70
Daniel Gross <dan.12sq@gmail.com>
2018-12-15 06:28:21 +0000

    day 15 - up to identify targets

--- day15.py
+++ day15.py
@@ -33,6 +33,69 @@ print()
 print('PART ONE')
 ans = None
 
+HP = 200
+AP = 3
+class Goblin():
+    def __init__(self, x, y):
+        self.hp = HP
+        self.x = x
+        self.y = y
+    def __str__(self):
+        return 'G'
+class Elf():
+    def __init__(self, x, y):
+        self.hp = HP
+        self.x = x
+        self.y = y
+    def __str__(self):
+        return 'E'
+
+goblins = []
+elves = []
+board = []
+for y in range(len(inputs)):
+    line = inputs[y]
+    board.append([])
+    for x in range(len(line)):
+        c = line[x]
+        if c in '.#':
+            board[-1].append(c)
+        elif c == 'G':
+            g = Goblin(x, y)
+            board[-1].append(g)
+            goblins.append(g)
+        elif c == 'E':
+            e = Elf(x, y)
+            board[-1].append(e)
+            elves.append(e)
+
+for line in board:
+    print(''.join(map(str, line)))
+
+print(elves)
+print(goblins)
+while len(elves) and len(goblins):
+    for y in range(len(board)):
+        for x in range(len(board[0])):
+            if type(board[y][x]) in {Goblin, Elf}:
+                print(board[y][x])
+                target_locs = []
+                target_array = goblins if type(board[y][x]) == Elf else elves
+                for target in target_array:
+                    tx = target.x
+                    ty = target.y
+                    for xoff, yoff in {(-1, 0), (0, -1), (0, 1), (1, 0)}:
+                        if board[ty+yoff][tx+xoff] == '.':
+                            target_locs.append((ty+yoff, tx+xoff))
+                            board[ty+yoff][tx+xoff] = '?'
+                break
+        else:
+            continue
+        break
+    break
+
+for line in board:
+    print(''.join(map(str, line)))
 
 
 print(ans)
