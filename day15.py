6c386dafbd2bb59b72d134cc02f12de5f5905c73
Daniel Gross <dan.12sq@gmail.com>
2018-12-15 06:28:44 +0000

    day 15 - up to choosing a target location

--- day15.py
+++ day15.py
@@ -69,16 +69,27 @@ for y in range(len(inputs)):
             board[-1].append(e)
             elves.append(e)
 
-for line in board:
-    print(''.join(map(str, line)))
+def reset_dboard():
+    global board
+    dboard = []
+    for y in range(len(board)):
+        dboard.append([])
+        for x in range(len(board[0])):
+            dboard[-1].append(None if str(board[y][x]) in 'EG#' else 0)
+    return dboard
 
-print(elves)
-print(goblins)
+def show_board():
+    global board
+    for line in board:
+        print(''.join(map(str, line)))
+
+show_board()
+
 while len(elves) and len(goblins):
     for y in range(len(board)):
         for x in range(len(board[0])):
             if type(board[y][x]) in {Goblin, Elf}:
-                print(board[y][x])
+                # identify all targets and "in range" points (?)
                 target_locs = []
                 target_array = goblins if type(board[y][x]) == Elf else elves
                 for target in target_array:
@@ -86,18 +97,67 @@ while len(elves) and len(goblins):
                     ty = target.y
                     for xoff, yoff in {(-1, 0), (0, -1), (0, 1), (1, 0)}:
                         if board[ty+yoff][tx+xoff] == '.':
-                            target_locs.append((ty+yoff, tx+xoff))
+                            target_locs.append((tx+xoff, ty+yoff))
                             board[ty+yoff][tx+xoff] = '?'
+                show_board()
+
+                # find distance to all reachable points (@)
+                dboard = reset_dboard()
+                points = [(x, y)]
+                d = 0
+                min_d = None
+                while points:
+                    npoints = []
+                    for point in points:
+                        px, py = point
+                        if d > 0:
+                            dboard[py][px] = d
+                            if str(board[py][px]) == '?':
+                                board[py][px] = '@'
+
+                        for xoff, yoff in {(-1, 0), (0, -1), (0, 1), (1, 0)}:
+                            if not dboard[py+yoff][px+xoff] and str(board[py+yoff][px+xoff]) in '.?' and not (x == px + xoff and y == py + yoff):
+                                npoints.append((px+xoff, py+yoff))
+                    points = npoints
+                    d += 1
+                show_board()
+                print(dboard)
+
+                # narrow scope to all nearest points (!)
+                for lx, ly in target_locs:
+                    d = dboard[ly][lx]
+                    if d == 0 or d is None:
+                        continue
+                    if min_d is None or d in range(1, min_d):
+                       min_d = d
+
+                closest_locs = []
+                for lx, ly in target_locs:
+                    if dboard[ly][lx] == min_d:
+                        board[ly][lx] = str(dboard[ly][lx])
+                        closest_locs.append((lx, ly))
+                    else:
+                        board[ly][lx] = '.'
+                print(dboard)
+                print(closest_locs)
+                show_board()
+
+                # choose the point
+                min_y = min(closest_locs, key=lambda x: x[1])[1]
+                print(min_y)
+                chosen = sorted((list(filter(lambda x: x[1] == min_y, closest_locs))))[0]
+                for lx, ly in closest_locs:
+                    if (lx, ly) == chosen:
+                        board[ly][lx] = '+'
+                    else:
+                        board[ly][lx] = '.'
                 break
         else:
             continue
         break
     break
 
-for line in board:
-    print(''.join(map(str, line)))
-
-
+show_board()
 print(ans)
 if testing:
     if part_one == ans:
