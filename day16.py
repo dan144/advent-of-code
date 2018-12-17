b3f4ed3b33607dd4056c488818b52e8c9c4780ce
Daniel Gross <dan.12sq@gmail.com>
2018-12-17 00:00:58 +0000

    day 16 - part 1 solution

--- day16.py
+++ day16.py
@@ -33,8 +33,101 @@ print()
 print('PART ONE')
 ans = None
 
+#Before: [2, 3, 2, 2]
+#0 3 3 0
+#After:  [0, 3, 2, 2]
 
+opsets = []
+parsing = True
+skipped = False
+while parsing and inputs:
+    line = inputs.pop(0)
+    if line == '':
+        if skipped:
+            parsing = False
+        skipped = True
+    elif line.startswith('Before:'):
+        op = []
+        op.append(list(map(int, line.split('[', 1)[1][:-1].split(', '))))
+        line = inputs.pop(0)
+        op.append(list(map(int, line.split(' '))))
+        line = inputs.pop(0)
+        op.append(list(map(int, line.split('[', 1)[1][:-1].split(', '))))
+        skipped = False
+        opsets.append(op)
 
+def addr(b, o, a):
+    return a[o[3]] == (b[o[1]] + b[o[2]])
+
+def addi(b, o, a):
+    return a[o[3]] == (b[o[1]] + o[2])
+    
+def mulr(b, o, a):
+    return a[o[3]] == (b[o[1]] * b[o[2]])
+    
+def muli(b, o, a):
+    return a[o[3]] == (b[o[1]] * o[2])
+
+def banr(b, o, a):
+    return a[o[3]] == (b[o[1]] & b[o[2]])
+
+def bani(b, o, a):
+    return a[o[3]] == (b[o[1]] & o[2])
+
+def borr(b, o, a):
+    return a[o[3]] == (b[o[1]] | b[o[2]])
+
+def bori(b, o, a):
+    return a[o[3]] == (b[o[1]] | o[2])
+
+def setr(b, o, a):
+    return a[o[3]] == b[o[1]]
+
+def seti(b, o, a):
+    return a[o[3]] == o[1]
+
+def gtir(b, o, a):
+    return a[o[3]] == (1 if (o[1] > b[o[2]]) else 0)
+
+def gtri(b, o, a):
+    return a[o[3]] == (1 if (b[o[1]] > o[2]) else 0)
+
+def gtrr(b, o, a):
+    return a[o[3]] == (1 if (b[o[1]] > b[o[2]]) else 0)
+
+def eqir(b, o, a):
+    return a[o[3]] == (1 if (o[1] == b[o[2]]) else 0)
+
+def eqri(b, o, a):
+    return a[o[3]] == (1 if (b[o[1]] == o[2]) else 0)
+
+def eqrr(b, o, a):
+    return a[o[3]] == (1 if (b[o[1]] == b[o[2]]) else 0)
+
+op_fs = (addr, addi, mulr, muli, banr, bani, borr, bori, setr, seti, gtir, gtri, gtrr, eqir, eqri, eqrr)
+op_is = [op_fs] * 16
+def test_ops(opset):
+    before, op, after = opset
+    if testing:
+        print(before, op, after)
+
+    ops = 0
+    for op_f in op_fs:
+        if op_f(before, op, after):
+            if testing:
+                print(str(op_f))
+            ops += 1
+
+    return ops
+
+print(len(opsets))
+opcount = 0
+for op in opsets:
+    nops = test_ops(op)
+    if nops >= 3:
+        opcount += 1
+
+ans = opcount
 print(ans)
 if testing:
     if part_one == ans:
