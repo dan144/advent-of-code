041da9981b08826acf07aa02fbc424fdba522f3e
Daniel Gross <dan.12sq@gmail.com>
2018-12-17 00:14:45 +0000

    day 16 solution

--- day16.py
+++ day16.py
@@ -3,6 +3,8 @@
 import json
 import sys
 
+from copy import copy
+
 print('Running:',sys.argv[0])
 
 testing = len(sys.argv) == 2
@@ -56,56 +58,58 @@ while parsing and inputs:
         skipped = False
         opsets.append(op)
 
-def addr(b, o, a):
-    return a[o[3]] == (b[o[1]] + b[o[2]])
+def addr(b, o):
+    return (b[o[1]] + b[o[2]])
 
-def addi(b, o, a):
-    return a[o[3]] == (b[o[1]] + o[2])
+def addi(b, o):
+    return (b[o[1]] + o[2])
     
-def mulr(b, o, a):
-    return a[o[3]] == (b[o[1]] * b[o[2]])
+def mulr(b, o):
+    return (b[o[1]] * b[o[2]])
     
-def muli(b, o, a):
-    return a[o[3]] == (b[o[1]] * o[2])
+def muli(b, o):
+    return (b[o[1]] * o[2])
 
-def banr(b, o, a):
-    return a[o[3]] == (b[o[1]] & b[o[2]])
+def banr(b, o):
+    return (b[o[1]] & b[o[2]])
 
-def bani(b, o, a):
-    return a[o[3]] == (b[o[1]] & o[2])
+def bani(b, o):
+    return (b[o[1]] & o[2])
 
-def borr(b, o, a):
-    return a[o[3]] == (b[o[1]] | b[o[2]])
+def borr(b, o):
+    return (b[o[1]] | b[o[2]])
 
-def bori(b, o, a):
-    return a[o[3]] == (b[o[1]] | o[2])
+def bori(b, o):
+    return (b[o[1]] | o[2])
 
-def setr(b, o, a):
-    return a[o[3]] == b[o[1]]
+def setr(b, o):
+    return b[o[1]]
 
-def seti(b, o, a):
-    return a[o[3]] == o[1]
+def seti(b, o):
+    return o[1]
 
-def gtir(b, o, a):
-    return a[o[3]] == (1 if (o[1] > b[o[2]]) else 0)
+def gtir(b, o):
+    return (1 if (o[1] > b[o[2]]) else 0)
 
-def gtri(b, o, a):
-    return a[o[3]] == (1 if (b[o[1]] > o[2]) else 0)
+def gtri(b, o):
+    return (1 if (b[o[1]] > o[2]) else 0)
 
-def gtrr(b, o, a):
-    return a[o[3]] == (1 if (b[o[1]] > b[o[2]]) else 0)
+def gtrr(b, o):
+    return (1 if (b[o[1]] > b[o[2]]) else 0)
 
-def eqir(b, o, a):
-    return a[o[3]] == (1 if (o[1] == b[o[2]]) else 0)
+def eqir(b, o):
+    return (1 if (o[1] == b[o[2]]) else 0)
 
-def eqri(b, o, a):
-    return a[o[3]] == (1 if (b[o[1]] == o[2]) else 0)
+def eqri(b, o):
+    return (1 if (b[o[1]] == o[2]) else 0)
 
-def eqrr(b, o, a):
-    return a[o[3]] == (1 if (b[o[1]] == b[o[2]]) else 0)
+def eqrr(b, o):
+    return (1 if (b[o[1]] == b[o[2]]) else 0)
 
-op_fs = (addr, addi, mulr, muli, banr, bani, borr, bori, setr, seti, gtir, gtri, gtrr, eqir, eqri, eqrr)
-op_is = [op_fs] * 16
+op_fs = [addr, addi, mulr, muli, banr, bani, borr, bori, setr, seti, gtir, gtri, gtrr, eqir, eqri, eqrr]
+op_is = []
+for i in range(16):
+    op_is.append(copy(op_fs))
 def test_ops(opset):
     before, op, after = opset
     if testing:
@@ -113,14 +117,15 @@ def test_ops(opset):
 
     ops = 0
     for op_f in op_fs:
-        if op_f(before, op, after):
+        if op_f(before, op) == after[op[3]]:
             if testing:
                 print(str(op_f))
             ops += 1
+        elif op_f in op_is[op[0]]:  # this opcode could be this fuction
+            op_is[op[0]].remove(op_f)
 
     return ops
 
-print(len(opsets))
 opcount = 0
 for op in opsets:
     nops = test_ops(op)
@@ -141,7 +146,37 @@ print('PART TWO')
 ans = None
 
 
+def claim_op(i):
+    for j in range(len(op_is)):
+        if i != j and op_is[i][0] in op_is[j]:
+            op_is[j].remove(op_is[i][0])
+            if len(op_is[j]) == 1:
+                claim_op(j)
 
+
+for i in range(len(op_is)):
+    if len(op_is[i]) == 1:
+        claim_op(i)
+
+for i in range(len(op_is)):
+    if len(op_is[i]) == 1:
+        op_is[i] = op_is[i][0]
+        if testing:
+            print(op_is[i])
+    else:
+        print('More than one possible opcode for opcode', i, '-', op_is[i])
+        sys.exit(1)
+
+cmds = []
+for line in inputs:
+    if line != '':
+        cmds.append(list(map(int, line.split())))
+
+r = [0, 0, 0, 0]
+for cmd in cmds:
+    r[cmd[3]] = op_is[cmd[0]](r, cmd)
+
+ans = r[0]
 print(ans)
 if testing:
     if part_two == ans:
