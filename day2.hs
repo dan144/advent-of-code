ef5a25cbf9832c9673012c2893d6dd93e96408cb
Daniel Gross <dan.12sq@gmail.com>
2018-12-18 18:41:06 +0000

    day 1-2 refactor - split days into separate haskell

--- day2.hs
+++ day2.hs
@@ -5,24 +5,10 @@ import Data.List.Split
 
 main :: IO ()
 main = do
-    print $ "Run a daily module, dayN"
-
-day1 = do
-    contents <- readFile "input/1"
-
-    print $ "PART ONE"
-    print $ sum $ map converter contents
-    print $ "PART TWO"
-    print $ find_basement contents
-
-find_basement contents = length $ takeWhile (/= (-1)) $ scanl (+) 0 $ map converter contents
-converter '(' = 1
-converter ')' = -1
-converter _ = 0
-
-day2 = do
     contents <- readFile "input/2"
+    print $ "PART ONE"
     print $ sum $ map wrapping_paper $ splitOn "\n" contents
+    print $ "PART TWO"
     print $ sum $ map ribbon $ splitOn "\n" contents
 
 get_size [a,b,c] = 3*a*b + 2*a*c + 2*b*c
