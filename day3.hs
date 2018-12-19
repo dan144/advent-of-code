ab374d1aa2a6072ed280b6bf9ec3875910cbeb32
Daniel Gross <dan.12sq@gmail.com>
2018-12-19 04:56:29 +0000

    day 3 part 2 solution

--- day3.hs
+++ day3.hs
@@ -7,12 +7,20 @@ main :: IO ()
 main = do
     contents <- readFile "input/3"
     print $ "PART ONE"
-    print $ length $ Data.Sort.uniqueSort $ moves contents
+    print $ length $ Data.Sort.uniqueSort $ locations contents
     print $ "PART TWO"
+    let santa_moves = every_other contents
+    let robo_moves = every_other $ drop 1 contents
 
-moves contents = drop 1 $ scanl (sum_them) [0,0] $ map converter contents
+    print $ length $ Data.Sort.uniqueSort $ (locations santa_moves ++ locations robo_moves)
+
+locations contents = drop 1 $ scanl (sum_them) [0,0] $ map converter contents
+
+every_other (x:y:xs) = x : every_other xs
+every_other _ = []
 
 sum_them x y = zipWith (+) x y
+
 converter 'v' = [1,0]
 converter '^' = [-1,0]
 converter '<' = [0,-1]
