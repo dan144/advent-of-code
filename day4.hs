3c7338969129e4a875c5995830046dd648042fd8
Daniel Gross <dan.12sq@gmail.com>
2018-12-20 00:53:29 +0000

    day 4 part 2 solution

--- day4.hs
+++ day4.hs
@@ -11,6 +11,7 @@ main = do
     print $ "PART ONE"
     print $ get_index $ map_hash $ map_num (B.init contents)
     print $ "PART TWO"
+    print $ get_extra_index $ map_hash $ map_num (B.init contents)
 
 add_num s n = B.concat [s, toByteString n]
 add_num _ _ = toByteString ""
@@ -22,4 +23,8 @@ map_hash vs = Prelude.map get_hash vs
 is_not_valid ('0':'0':'0':'0':'0':xs) = False
 is_not_valid _ = True
 
+is_not_extra_valid ('0':'0':'0':'0':'0':'0':xs) = False
+is_not_extra_valid _ = True
+
 get_index hashes = (\x -> x +1) $ Prelude.length $ Prelude.takeWhile (is_not_valid) hashes
+get_extra_index hashes = (\x -> x +1) $ Prelude.length $ Prelude.takeWhile (is_not_extra_valid) hashes
