7bd484e1400e3c33ed811678df00d4f83fb438fa
Daniel Gross <dan.12sq@gmail.com>
2018-12-22 08:33:22 +0000

    day 22 part 2 solution

--- day22.py
+++ day22.py
@@ -3,6 +3,8 @@
 import json
 import sys
 
+from copy import copy
+
 print('Running:',sys.argv[0])
 
 testing = len(sys.argv) == 2
@@ -29,18 +31,36 @@ with open(input_file, 'r') as f:
 if testing:
     print(inputs)
 
+depth = int(inputs[0].split()[1])
+tx, ty = list(map(int, inputs[1].split()[1].split(',')))
+xm = 2
+print('(For part 2, running with max x value at {}x target x value)'.format(xm))
+maxx = int(tx * xm)  # determine a better way to detect the max
+maxy = int(ty * 2)
+
 print()
 print('PART ONE')
 ans = None
 
-depth = int(inputs[0].split()[1])
-tx, ty = list(map(int, inputs[1].split()[1].split(',')))
+# Times for various region multipliers
+# 200,3 -> 1m37s
+# 100,2 -> 1m27s
+# 50,2  -> 0m33s
+# 10,2  -> 0m09s
+# 10,1  -> 0m09s
+# 5,1   -> 0m08s
+
+minm = None
+# FAILS
+# 1076 - too high
+# 1069 - too low
+# 1075 - right answer
 
 cave = []
 risk_level = 0
-for y in range(ty+1):
+for y in range(maxy+1):
     cave.append([])
-    for x in range(tx+1):
+    for x in range(maxx+1):
         if x == 0 and y == 0:
             geologic = 0
         elif x == tx and y == ty:
@@ -53,7 +73,8 @@ for y in range(ty+1):
             geologic = cave[y][x-1] * cave[y-1][x]
         erosion = (geologic + depth) % 20183
         cave[y].append(erosion)
-        risk_level += (erosion % 3)
+        if x <= tx and y <= ty:
+            risk_level += (erosion % 3)
 
 ans = risk_level
 print(ans)
@@ -68,8 +89,83 @@ print()
 print('PART TWO')
 ans = None
 
+def move(spots):
+    global mcosts
+    n = []
+    for spot in spots:
+        if minm is not None and spot[3] >= minm:
+            continue
+        x, y, equipped, ocost, seen = spot
+        # this check should never be necessary
+        #if x == tx and y == ty:
+        #    cost = 0 if equipped == TORCH else 7
+        #    n.append((x, y, TORCH, ocost + cost, None))
+        #    continue
+        for dx, dy in {(-1, 0), (0, -1), (0, 1), (1, 0)}:
+            if x + dx < 0 or y + dy < 0 or x + dx > maxx or y + dy > maxy or (x + dx, y + dy) in seen:
+                continue
+
+            if cave[y+dy][x+dx] % 3 == ROCKY:
+                next_set = {(WET, TORCH), (NARROW, CLIMBING)}
+            elif cave[y+dy][x+dx] % 3 == WET:
+                next_set = {(ROCKY, NEITHER), (NARROW, CLIMBING)}
+            elif cave[y+dy][x+dx] % 3 == NARROW:
+                next_set = {(ROCKY, NEITHER), (WET, TORCH)}
+
+            for rtype, nequip in next_set:
+                if cave[y][x] == rtype:  # the current region doesn't allow this equipment
+                    continue
+                cost = 1 if equipped == nequip else 8
+                if minm is None or ocost + cost < minm:
+                    if mcosts[nequip][y+dy][x+dx] is None or ocost + cost < mcosts[nequip][y+dy][x+dx]:
+                        mcosts[nequip][y+dy][x+dx] = ocost + cost
+                        nseen = copy(seen)
+                        nseen.add((x+dx, y+dy))
+                        n.append((x+dx, y+dy, nequip, ocost + cost, nseen))
+    return n
 
 
+# make the program more efficient by keeping track of the lowest seen cost of x/y/equip combos
+# mcosts is referenced by [equip][y][x]
+mcosts = []
+for e in range(3):
+    mcosts.append([])
+    for y in range(maxy+1):
+        mcosts[-1].append([])
+        for x in range(maxx+1):
+            mcosts[-1][y].append(None)
+
+ROCKY = 0   # torch, climbing
+WET = 1     # climbing, neither
+NARROW = 2  # torch, neither
+
+TORCH = 0
+CLIMBING = 1
+NEITHER = 2
+
+# x, y, equipped, cost, history
+spots = [(0, 0, TORCH, 0, {(0,0)})]
+left = []
+print('     Left  Minimum')
+while spots or left:
+    for spot in spots:
+        if spot[0] == tx and spot[1] == ty:
+            cost = spot[3] + (0 if spot[2] == TORCH else 7)
+            if minm is None or cost < minm:
+                minm = cost
+        else:
+            if minm is None or spot[3] < minm:
+                left.append(spot)
+    left.sort(key=lambda x: x[3], reverse=True)
+    spots = []
+    for i in range(300):  # sending the whole spots list at once gets slow: chunk it
+        if left:
+            spots.append(left.pop())
+    spots = move(spots)
+    print(' {:8d} {:>8s}\r'.format(len(spots) + len(left), str(minm) if minm is not None else '?'), end='')
+
+print()
+ans = minm
 print(ans)
 if testing:
     if part_two == ans:
