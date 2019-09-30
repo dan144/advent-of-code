50cc62a47b31001b0eae5d7739295b47085a3a5c
Daniel Gross <dan.12sq@gmail.com>
2019-09-30 17:12:55 +0000

    day 13 part 2

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -2,6 +2,25 @@ use permutohedron::Heap;
 use regex::Regex;
 use std::collections::HashMap;
 
+fn reduce(map : HashMap <String, HashMap<String, i32>>) -> i32 {
+    let mut name_vec = map.keys().cloned().collect::<Vec<String>>();
+    let heap = Heap::new(&mut name_vec);
+    let mut max : i32 = 0;
+    for p in heap {
+        let mut total : i32 = 0;
+        for x in 0..p.len() {
+            let prev_idx = x.checked_sub(1).unwrap_or(p.len() - 1);
+            let x_p = *map.get(&p[x]).unwrap().get(&p[prev_idx]).unwrap();
+            let p_x = *map.get(&p[prev_idx]).unwrap().get(&p[x]).unwrap();
+            total += x_p + p_x;
+        }
+        if total > max {
+            max = total;
+        }
+    }
+    max
+}
+
 fn main() -> std::io::Result<()> {
     use std::fs::File;
     use std::io::BufReader;
@@ -28,22 +47,15 @@ fn main() -> std::io::Result<()> {
         map.entry(person.to_string()).or_insert(Default::default()).insert(cause.to_string(), value);
         line.clear();
     };
+    println!("Part 1: {:?}", reduce(map.clone()));
 
-    let mut name_vec = map.keys().cloned().collect::<Vec<String>>();
-    let heap = Heap::new(&mut name_vec);
-    let mut max : i32 = 0;
-    for p in heap {
-        let mut total : i32 = 0;
-        for x in 0..p.len() {
-            let prev_idx = x.checked_sub(1).unwrap_or(p.len() - 1);
-            let x_p = *map.get(&p[x]).unwrap().get(&p[prev_idx]).unwrap();
-            let p_x = *map.get(&p[prev_idx]).unwrap().get(&p[x]).unwrap();
-            total += x_p + p_x;
-        }
-        if total > max {
-            max = total;
-        }
+    let name_vec = map.keys().cloned().collect::<Vec<String>>();
+    map.entry("Daniel".to_string()).or_insert(Default::default());
+    for name in name_vec {
+        map.entry(name.to_string()).or_insert(Default::default()).insert("Daniel".to_string(), 0);
+        map.entry("Daniel".to_string()).or_insert(Default::default()).insert(name.to_string(), 0);
     }
-    println!("Part 1: {:?}", max);
+    println!("Part 2: {:?}", reduce(map.clone()));
+
     Ok(())
 }
