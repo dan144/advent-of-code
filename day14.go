eec37686e10ab53f595be6db27453aae780aeae0
Daniel Gross <dan.12sq@gmail.com>
2019-10-01 17:34:50 +0000

    day 14 part 2

--- day14.go
+++ day14.go
@@ -20,7 +20,10 @@ func main() {
 	scanner := bufio.NewScanner(file)
 	s := ""
     seconds := 2503
-    max := float64(0)
+    var speeds [][]int
+    var points []int
+    var dist []int
+
 	for scanner.Scan() {
 		s = scanner.Text()
         re := regexp.MustCompile(`\d+`)
@@ -33,16 +36,48 @@ func main() {
             panic(err)
         }
 
-        fulls := seconds / (time + rest)
-        last := math.Mod(float64(seconds), float64(time + rest))
-        dist := (float64(fulls * time) + math.Min(float64(time), last)) * float64(speed)
-        if dist > max {
-            max = dist;
-        }
+        speeds = append(speeds, []int{speed, time, rest})
+        points = append(points, 0)
+        dist = append(dist, 0)
     }
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("Part 1: %d\n", int(max))
+
+    for sec := 0; sec <= seconds; sec++ {
+        for i, r := range speeds {
+            m_sec := math.Mod(float64(sec), float64(r[1] + r[2]))
+            if int(m_sec) < r[1] {
+                dist[i] += r[0]
+            }
+        }
+        max := 0
+        var winners []int
+        for i, _ := range speeds {
+            if dist[i] == dist[max] {
+               winners = append(winners, i)
+            } else if dist[i] > dist[max] {
+               winners = []int{i}
+               max = i
+            }
+        }
+        for _, m := range winners {
+            points[m] += 1
+        }
+    }
+
+    d_max := 0
+    p_max := 0
+    for i, _ := range speeds {
+        if dist[i] > d_max {
+           d_max = dist[i]
+        }
+        if points[i] > p_max {
+           p_max = points[i]
+        }
+    }
+
+	fmt.Printf("Part 1: %d\n", int(d_max))
+    fmt.Printf("Part 2: %d\n", int(p_max))
 }
