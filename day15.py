d0375fac79c00639e3dc5d955ae8cae3f0e0d11d
Daniel Gross <dan.12sq@gmail.com>
2019-10-20 12:59:24 -0400

    day 15 part 2

--- day15.py
+++ day15.py
@@ -9,7 +9,8 @@ with open('input/15', 'r') as f:
         print(m.groups())
         ing.append(m.groups())
 
-m = 0
+m_1 = 0
+m_2 = 0
 for a in range(101):
     for b in range(0, 101-a):
         for c in range(0, 101-a-b):
@@ -18,13 +19,19 @@ for a in range(101):
                 dur_n = 0
                 fla_n = 0
                 tex_n = 0
+                cal = 0
                 e = (a,b,c,d)
                 for f in range(4):
                     cap_n += e[f] * int(ing[f][1])
                     dur_n += e[f] * int(ing[f][2])
                     fla_n += e[f] * int(ing[f][3])
                     tex_n += e[f] * int(ing[f][4])
+                    cal += e[f] * int(ing[f][5])
                 if any((cap_n <= 0, dur_n <= 0, fla_n <= 0, tex_n <= 0)):
                     continue
-                m = max(m, cap_n * dur_n * fla_n * tex_n)
-print('Part 1: {}'.format(m))
+                m_1 = max(m_1, cap_n * dur_n * fla_n * tex_n)
+                if cal != 500:
+                    continue
+                m_2 = max(m_2, cap_n * dur_n * fla_n * tex_n)
+print('Part 1: {}'.format(m_1))
+print('Part 2: {}'.format(m_2))
