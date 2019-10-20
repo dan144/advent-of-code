47f343bd32a819408a25c3d74ece8aab516cf2e1
Daniel Gross <dan.12sq@gmail.com>
2019-10-20 13:07:24 -0400

    day 15 refactor

--- day15.py
+++ day15.py
@@ -6,8 +6,10 @@ ing = []
 with open('input/15', 'r') as f:
     for line in f:
         m = re.search(r'(\w+): capacity (-?\d+), durability (-?\d+), flavor (-?\d+), texture (-?\d+), calories (-?\d+)', line)
-        print(m.groups())
-        ing.append(m.groups())
+        g = list(m.groups())
+        for i in range(1, 6):
+            g[i] = int(g[i])
+        ing.append(g)
 
 m_1 = 0
 m_2 = 0
@@ -15,23 +17,16 @@ for a in range(101):
     for b in range(0, 101-a):
         for c in range(0, 101-a-b):
             for d in range(0, 101-a-b-c):
-                cap_n = 0
-                dur_n = 0
-                fla_n = 0
-                tex_n = 0
-                cal = 0
+                v = [0] * 5
                 e = (a,b,c,d)
                 for f in range(4):
-                    cap_n += e[f] * int(ing[f][1])
-                    dur_n += e[f] * int(ing[f][2])
-                    fla_n += e[f] * int(ing[f][3])
-                    tex_n += e[f] * int(ing[f][4])
-                    cal += e[f] * int(ing[f][5])
-                if any((cap_n <= 0, dur_n <= 0, fla_n <= 0, tex_n <= 0)):
+                    for i in range(5):
+                        v[i] += e[f] * ing[f][i+1]
+                if min(v[:4]) <= 0:
                     continue
-                m_1 = max(m_1, cap_n * dur_n * fla_n * tex_n)
-                if cal != 500:
+                m_1 = max(m_1, v[0] * v[1] * v[2] * v[3])
+                if v[4] != 500:
                     continue
-                m_2 = max(m_2, cap_n * dur_n * fla_n * tex_n)
+                m_2 = max(m_2, v[0] * v[1] * v[2] * v[3])
 print('Part 1: {}'.format(m_1))
 print('Part 2: {}'.format(m_2))
