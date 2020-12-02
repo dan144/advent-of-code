1475d08baf5c86889da195bf5ae07147311607bc
Daniel Gross <dan.12sq@gmail.com>
2020-12-02 00:23:08 -0500

    simplify day2

--- 2.py
+++ 2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import re
 import sys
 
 test = len(sys.argv) > 1
@@ -8,21 +9,18 @@ input_file = 'input' + sys.argv[0].split('.')[1].lstrip('/') + ('.test' if test 
 p1 = 0
 p2 = 0
 
-inp = []
 with open(input_file, 'r') as f:
     for line in f:
         rng, letter, password = line.split()
         letter = letter.rstrip(':')
         mn, mx = map(int, rng.split('-'))
-        l_c = 0
-        for c in password:
-            if c == letter:
-                l_c += 1
-        if l_c >= mn and l_c <= mx:
-            p1 += 1
 
-        if (password[mn-1] == letter and password[mx-1] != letter) or (password[mn-1] != letter and password[mx-1] == letter):
-            p2 += 1
+        l_c = len(re.findall(letter, password))
+        p1 += int(l_c in range(mn, mx+1))
+
+        first = password[mn-1] == letter
+        last = password[mx-1] == letter
+        p2 += first ^ last
 
 print(f'Part 1: {p1}')
 print(f'Part 2: {p2}')
