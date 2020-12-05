bcae160167b57b087651ae712e6dd5e7a1f169bb
Daniel Gross <dan.12sq@gmail.com>
2020-12-05 12:24:32 -0500

    day 5 refactor

--- 5.py
+++ 5.py
@@ -14,43 +14,17 @@ with open(input_file, 'r') as f:
     for line in f:
         inp.append(line)
 
-ids = []
+ids = set()
 for line in inp:
-    mn = 0
-    mx = 127
-    for i in range(7):
-        c = line[i]
-        l = mx - mn + 1
-        l //= 2
-        if c == 'F':
-            # lower half
-            mx -= l
-        else:
-            mn += l
-    row = mn
-
-    mn = 0
-    mx = 7
-    for i in range(3):
-        c = line[i+7]
-        l = mx - mn + 1
-        l //= 2
-        if c == 'L':
-            mx -= l
-        else:
-            mn += l
-    col = mn
+    row = int(line[:7].replace('F', '0').replace('B', '1'), 2)
+    col = int(line[7:].replace('L', '0').replace('R', '1'), 2)
     seat = row * 8 + col
 
     p1 = max(p1, seat)
 
-    ids.append(seat)
+    ids.add(seat)
 
 print(f'Part 1: {p1}')
 
-ids = sorted(ids)
-for i in range(len(ids) - 1):
-    if ids[i] + 1 != ids[i+1]:
-        p2 = ids[i] + 1
-
+p2 = (set(range(min(ids), max(ids) + 1)) - ids).pop()
 print(f'Part 2: {p2}')
