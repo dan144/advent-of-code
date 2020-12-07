581504999bb7eb6fea51f9b0466671883565230e
Daniel Gross <dan.12sq@gmail.com>
2020-12-07 00:59:45 -0500

    day 7 cleanup

--- 7.py
+++ 7.py
@@ -16,28 +16,20 @@ with open(input_file, 'r') as f:
     for line in f.read().splitlines():
         inp.append(line)
 
-holder = re.compile(r'[a-z]+ [a-z]+')
-held = re.compile(r'([0-9]+) ([a-z ]+) bags?')
+# ex: light red bags contain 1 bright white bag, 2 muted yellow bags.
+holder_re = re.compile(r'[a-z]+ [a-z]+') # line starts with "<adjective> <color> bags"
+held_re = re.compile(r'([0-9]+) ([a-z ]+) bags?') # every occurrence of "<n> <adjective> bag(s)"
+
 bags = {}
 for line in inp:
-    h = holder.search(line).group()
-    bags[h] = {}
+    holder = holder_re.search(line).group()
+    bags[holder] = {}
     if 'no other' not in line:
-        g = held.findall(line)
-        for n, t in g:
-            bags[h][t] = int(n)
-
-check = set()
-for color in bags.keys():
-    check.add(color)
-
-checked = set()
+        for n, t in held_re.findall(line):
+            bags[holder][t] = int(n)
 
 def dig(color):
-    if color not in bags.keys():
-        return False
-
-    colors = {c for c, n in bags[color].items()}
+    colors = bags[color].keys()
     if 'shiny gold' in colors:
         return True
 
@@ -46,17 +38,16 @@ def dig(color):
             return True
     return False
 
-for color in check:
-    p1 += dig(color) != 0
-
+for color in bags.keys():
+    p1 += dig(color)
 print(f'Part 1: {p1}')
 
-def contain(color, x):
-    t = x
+def contain(color, num):
+    total = num
     for c, n in bags[color].items():
-        t += x * contain(c, n)
-    return t
+        # this level has the num of these bags times what they contain
+        total += num * contain(c, n)
+    return total
 
 p2 = contain('shiny gold', 1) - 1
-
 print(f'Part 2: {p2}')
