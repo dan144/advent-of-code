dece3312d66b266ad4ae531674306ace0fad0636
Daniel Gross <dan.12sq@gmail.com>
2020-12-07 09:45:33 -0500

    day 7: use list comprehension

--- 7.py
+++ 7.py
@@ -12,7 +12,7 @@ p1 = 0
 p2 = 0
 
 inp = []
-with open(input_file, 'r') as f:
+with open(input_file) as f:
     for line in f.read().splitlines():
         inp.append(line)
 
@@ -22,7 +22,7 @@ held_re = re.compile(r'([0-9]+) ([a-z ]+) bags?') # every occurrence of "<n> <ad
 
 bags = {}
 for line in inp:
-    holder = holder_re.search(line).group()
+    holder = holder_re.match(line).group()
     bags[holder] = {}
     if 'no other' not in line:
         for n, t in held_re.findall(line):
@@ -30,24 +30,14 @@ for line in inp:
 
 def dig(color):
     colors = bags[color].keys()
-    if 'shiny gold' in colors:
-        return True
+    return 'shiny gold' in colors or any((dig(color) for color in colors))
 
-    for color in colors:
-        if dig(color):
-            return True
-    return False
-
-for color in bags.keys():
-    p1 += dig(color)
+p1 = sum(dig(color) for color in bags.keys())
 print(f'Part 1: {p1}')
 
 def contain(color, num):
-    total = num
-    for c, n in bags[color].items():
-        # this level has the num of these bags times what they contain
-        total += num * contain(c, n)
-    return total
+    # this level has the num of these bags times what they contain
+    return num + sum(num * contain(c, n) for c, n in bags[color].items())
 
 p2 = contain('shiny gold', 1) - 1
 print(f'Part 2: {p2}')
