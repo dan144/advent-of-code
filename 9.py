ab75be897195a5d749ba71ed2fde1d4d570f673f
Daniel Gross <dan.12sq@gmail.com>
2020-12-09 08:10:30 -0500

    day 9

---
+++ 9.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+import sys
+
+from copy import copy
+
+test = len(sys.argv) > 1
+input_file = 'input' + sys.argv[0].split('.')[1].lstrip('/') + ('.test' if test else '')
+
+p1 = 0
+p2 = 0
+
+inp = []
+with open(input_file) as f:
+    for line in f:
+        inp.append(int(line))
+
+n_preamble = 5 if test else 25
+preamble = copy(inp[:n_preamble])
+
+for n in inp[n_preamble:]:
+    valid = False
+    for a in range(n_preamble):
+        for b in range(n_preamble):
+            if preamble[a] == preamble[b]:
+                continue
+            if preamble[a] + preamble[b] == n:
+                valid = True
+                break
+        else:
+            continue
+        # break
+    else:
+        preamble.pop(0)
+        preamble.append(n)
+    if not valid:
+        p1 = n
+        # break
+print(f'Part 1: {p1}')
+
+for start in range(len(inp)):
+    total = 0
+    i = 0
+    while total < p1:
+        total += inp[start + i]
+        i += 1
+    if total == p1:
+        p2 = min(inp[start:start+i]) + max(inp[start:start+i])
+        break
+
+print(f'Part 2: {p2}')
