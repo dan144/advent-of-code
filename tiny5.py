bd6f55f95ec7f6bfbd338cfe51a28be2e9146608
Daniel Gross <dan.12sq@gmail.com>
2020-12-10 17:18:03 -0500

    update input filename

--- tiny5.py
+++ tiny5.py
@@ -1,1 +1,1 @@
-print(max(ids := {int("".join(("0" if c in "FL" else "1" for c in line[:-1])), 2) for line in open("input5")}), (set(range(min(ids), max(ids))) - ids).pop())
+print(max(ids := {int("".join(("0" if c in "FL" else "1" for c in line[:-1])), 2) for line in open("input05")}), (set(range(min(ids), max(ids))) - ids).pop())
