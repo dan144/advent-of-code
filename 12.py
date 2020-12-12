adeb6a7fb20f7ef003048ff6bed21471ee94f426
Daniel Gross <dan.12sq@gmail.com>
2020-12-12 00:28:22 -0500

    day 12 cleanup

--- 12.py
+++ 12.py
@@ -1,15 +1,9 @@
 #!/usr/bin/env python3
 
-import re
 import sys
-
-from computer import parse, run
 
 test = len(sys.argv) > 1
 input_file = 'input' + sys.argv[0].split('.')[1].lstrip('/') + ('.test' if test else '')
-
-p1 = 0
-p2 = 0
 
 inp = []
 with open(input_file) as f:
@@ -17,42 +11,37 @@ with open(input_file) as f:
         inp.append((line[0], int(line[1:])))
 
 dirs = 'NESW'
+moves = {
+    'N': (0, 1),
+    'E': (1, 0),
+    'S': (0, -1),
+    'W': (-1, 0),
+    'R': 1,
+    'L': -1,
+}
+
 direc = 1
-y = 0
-x = 0
-wy = 10
-wx = 1
+x, y = 0, 0
 for act, dist in inp:
-    if act == 'R':
-        direc = (direc + (dist // 90)) % 4
-        act = dirs[direc]
-    elif act == 'L':
-        direc = (direc - (dist // 90)) % 4
+    if act in 'LR':
+        direc = (direc + moves[act] * dist // 90) % 4
         act = dirs[direc]
     else:
         if act == 'F':
             act = dirs[direc]
-        if act == 'N':
-            y += dist
-        elif act == 'S':
-            y -= dist
-        elif act == 'E':
-            x += dist
-        elif act == 'W':
-            x -= dist
+        dx, dy = moves[act]
+        x += dx * dist
+        y += dy * dist
 
 p1 = abs(x) + abs(y)
 print(f'Part 1: {p1}')
 
 direc = 1
-y = 0
-x = 0
-wy = 1
-wx = 10
+x, y = 0, 0
+wx, wy = 10, 1
 for act, dist in inp:
-    if act == 'R' or act == 'L':
-        turns = dist // 90
-        for t in range(turns):
+    if act in 'LR':
+        for t in range(dist // 90):
             if act == 'R':
                 s = wy
                 wy = -1 * wx
@@ -64,15 +53,10 @@ for act, dist in inp:
     elif act == 'F':
         x += dist * wx
         y += dist * wy
-
-    elif act == 'N':
-        wy += dist
-    elif act == 'S':
-        wy -= dist
-    elif act == 'E':
-        wx += dist
-    elif act == 'W':
-        wx -= dist
+    else:
+        dx, dy = moves[act]
+        wx += dx * dist
+        wy += dy * dist
 
 p2 = abs(x) + abs(y)
 print(f'Part 2: {p2}')
