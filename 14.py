6cd88fd113cfcf82df24be20b453f0705e7e35b2
Daniel Gross <dan.12sq@gmail.com>
2020-12-14 00:57:35 -0500

    day 14 comments and cleanup

--- 14.py
+++ 14.py
@@ -6,12 +6,13 @@ import sys
 test = len(sys.argv) > 1
 input_file = 'input' + sys.argv[0].split('.')[1].lstrip('/') + ('.test' if test else '')
 
-def fill(l_addr, val):
-    if 'X' in l_addr:
-        fill(l_addr.replace('X', '0', 1), val)
-        fill(l_addr.replace('X', '1', 1), val)
+def fill(addr, val):
+    # takes address as a binary string and recurses for every X to reach all addrs
+    if 'X' in addr:
+        fill(addr.replace('X', '0', 1), val)
+        fill(addr.replace('X', '1', 1), val)
     else:
-        addr = int(l_addr, 2)
+        addr = int(addr, 2)
         if addr not in mem:
             mem[addr] = 0
         mem[addr] = (omask & val) | zmask
@@ -21,16 +22,16 @@ with open(input_file) as f:
     for line in f:
         if line.startswith('mask = '):
             mask = line.split(' = ')[1].rstrip('\n')
-            zmask = int(mask.replace('X', '0'), 2)
-            omask = int(mask.replace('X', '1'), 2)
+            zmask = int(mask.replace('X', '0'), 2) # do not replace on bitwise or
+            omask = int(mask.replace('X', '1'), 2) # force replacement on bitwise and
         else:
             addr, val = map(int, re.findall(r'[0-9]+', line))
-            l_addr = '{0:036b}'.format(addr)
-            fill(l_addr, val)
+            fill(f'{addr:036b}', val)
 
 p1 = sum(mem.values())
 print(f'Part 1: {p1}')
 
+# all ones and zeros to make it a pass through in fill()
 omask = 2 ** 36 - 1
 zmask = 0
 mem = {}
@@ -39,8 +40,9 @@ with open(input_file) as f:
         if line.startswith('mask = '):
             mask = list(line.split(' = ')[1].rstrip('\n'))
         else:
-            addr, val = map(int, re.findall(r'[0-9]+', line))
-            l_addr = list('{0:036b}'.format(addr))
+            i_addr, val = map(int, re.findall(r'[0-9]+', line))
+            l_addr = list(f'{i_addr:036b}')
+            # overwrite X and 1 from mask, pass through from address on 0
             addr = ''.join((mask[b] if mask[b] in 'X1' else l_addr[b] for b in range(len(mask))))
             fill(addr, val)
 
