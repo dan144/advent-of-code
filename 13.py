d425660b68f7a095a4105e5e81ccfd4061de6926
Daniel Gross <dan.12sq@gmail.com>
2020-12-14 10:22:50 -0500

    day 13 simplification

--- 13.py
+++ 13.py
@@ -5,34 +5,27 @@ import sys
 test = len(sys.argv) > 1
 input_file = 'input' + sys.argv[0].split('.')[1].lstrip('/') + ('.test' if test else '')
 
-buses = []
-first = None
+earliest = None
 with open(input_file) as f:
-    first = int(f.readline())
-    for b in f.readline().rstrip('\n').split(','):
-        buses.append(b if b == 'x' else int(b))
+    earliest = int(f.readline())
+    inp = f.readline().rstrip('\n').split(',')
+    buses = {i: int(inp[i]) for i in range(len(inp)) if inp[i] != 'x'}
 
 low = (float('inf'), 0)
-for bus in buses:
-    if bus == 'x':
-        continue
-
+for bus in buses.values():
     t = bus
-    while t < first:
+    while t < earliest:
         t += bus
-    diff = t - first
-    if diff < low[0]:
-        low = (diff, bus)
+    diff = t - earliest
+    low = (diff, bus) if diff < low[0] else low
 
 p1 = low[0] * low[1]
 print(f'Part 1: {p1}')
 
 p2 = 0 if test else 100000000000000
 inc = 1
-for b in range(len(buses)):
-    if buses[b] == 'x':
-        continue
-    while (p2 + b) % buses[b] != 0:
+for b, bus in buses.items():
+    while (p2 + b) % bus != 0:
         p2 += inc
-    inc *= buses[b]
+    inc *= bus
 print(f'Part 2: {p2}')
