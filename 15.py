1e4ab73bb247786924c0b2792fa79b23ed156dc3
Daniel Gross <dan.12sq@gmail.com>
2020-12-15 00:38:52 -0500

    day 15 cleanup: init outside main loop

--- 15.py
+++ 15.py
@@ -1,29 +1,21 @@
 #!/usr/bin/env python3
 
-import re
 import sys
-
-from computer import parse, run
 
 test = len(sys.argv) > 1
 input_file = 'input' + sys.argv[0].split('.')[1].lstrip('/') + ('.test' if test else '')
-
-p1 = 0
-p2 = 0
 
 with open(input_file) as f:
     nums = list(map(int, [n for n in f.readline().rstrip('\n').split(',')]))
 
 spoken = {}
-for i in range(30000000):
-    if nums:
-        n = nums.pop(0)
-    else:
-        arr = spoken.get(n, [])
-        if len(arr) <= 1:
-            n = 0
-        else:
-            n = abs(spoken[n][-1] - spoken[n][-2])
+for i, n in enumerate(nums):
+    spoken[n] = spoken.get(n, [])
+    spoken[n].append(i)
+
+for i in range(len(nums), 30000000):
+    arr = spoken.get(n, [])
+    n = abs(arr[-1] - arr[-2]) if len(arr) > 1 else 0
 
     if n in spoken:
         spoken[n].append(i)
