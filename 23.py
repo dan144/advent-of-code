485fca5b191130a62790d5063900a961ec95f971
Daniel Gross <dan.12sq@gmail.com>
2020-12-23 01:41:43 -0500

    day 23 (part 2 only)

--- 23.py
+++ 23.py
@@ -3,8 +3,6 @@
 import re
 import sys
 
-from computer import parse, run
-
 test = len(sys.argv) > 1
 input_file = 'input' + sys.argv[0].split('.')[1].lstrip('/') + ('.test' if test else '')
 
@@ -13,48 +11,62 @@ p2 = 0
 
 with open(input_file) as f:
     val = list(map(int, list(f.readline()[:-1])))
+
 mx = max(val)
-while len(val) < 1000000:
-    val.append(mx + 1)
-    mx = max(val)
+data = {}
+for i, v in enumerate(val[:-1]):
+    data[v] = val[i+1]
+data[val[i+1]] = mx + 1
+go_to = 1000000
+data.update({x: x + 1 for x in range(mx + 1, go_to)})
+data[go_to] = val[0]
+if go_to < 100:
+    print(data)
+mx = go_to
+
+def show(start):
+    n = None
+    while n != start:
+        n = start if n is None else n
+        print(n, end=' ')
+        n = data[n]
+    print()
 
 next_cup = val[0]
-for move in range(10000000): #10 if test else 100):
-    look_at = val.index(next_cup)
-    if move % 100 == 0:
+for move in range(10000000):
+    if move % 10000 == 0:
         print('Move:', move)
-    current = val[look_at]
-    #print('cups:', val, f'({current})')
-    n = 3
+    current = next_cup
+    # show(current)
+    #print(f'({current})')
     new = []
-    for i in range(n):
-        idx = look_at + 1
-        if idx >= len(val):
-            idx = 0
-        n = val.pop(idx)
+    n = current
+    for i in range(3):
+        n = data[n]
         new.append(n)
-    idx = look_at + 1
-    if idx >= len(val):
-        idx = 0
-    next_cup = val[idx]
+    next_cup = data[new[-1]]
+    data[current] = next_cup
     #print('pick up:', new)
+    dest = current
     while True:
         try:
-            put_at = val.index(current - 1)
-            break
-        except ValueError:
-            current -= 1
-            if current < 0:
-                current = mx + 1
-    dest = val[put_at]
-    #print('destination:', val[put_at])
-    idx = val.index(dest)
-    val = val[:idx+1] + new + val[idx+1:]
+            dest = dest - 1
+            if data[dest] and dest not in new:
+                break
+        except KeyError:
+            dest -= 1
+            if dest < 0:
+                dest = mx + 1
+    #print('destination:', dest)
+    data[new[-1]] = data[dest]
+    data[dest] = new[0]
+    # input()
 
-idx = val.index(1)
-p2 = val[idx + 1] * val[idx + 2]
+# idx = val.index(1)
 # p1 = ''.join(map(str, val[idx+1:] + val[:idx]))
+# show(1)
 print(f'Part 1: {p1}')
 
-
+print(data[1], data[data[1]])
+p2 = data[1] * data[data[1]]
 print(f'Part 2: {p2}')
