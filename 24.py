e7929394f18128238658baaf51f8d7e96ef36132
Daniel Gross <dan.12sq@gmail.com>
2020-12-24 00:48:54 -0500

    day 24 cleanup

--- 24.py
+++ 24.py
@@ -6,12 +6,14 @@ import sys
 test = len(sys.argv) > 1
 input_file = 'input' + sys.argv[0].split('.')[1].lstrip('/') + ('.test' if test else '')
 
-inp = []
 with open(input_file) as f:
-    for line in f:
-        inp.append(line[:-1])
+    inp = {line[:-1] for line in f}
 
+WHITE = False
+BLACK = True
 poss_moves = {'e', 'se', 'sw', 'w', 'nw', 'ne'}
+
+# moves in a hex grid
 change = {
     'e': (2, 0),
     'se': (1, -1),
@@ -20,53 +22,45 @@ change = {
     'nw': (-1, 1),
     'ne': (1, 1),
 }
-tiles = {}
 
+def surround_all(grid):
+    for (x, y) in list(grid.keys()):
+        for dx, dy in change.values():
+            nx, ny = x + dx, y + dy
+            if (nx, ny) not in grid:
+                grid[nx, ny] = WHITE
+
+grid = {}
 for line in inp:
-    moves = re.findall('|'.join(poss_moves), line)
+    moves = re.findall('|'.join(poss_moves), line) # greedy regex
     x, y = 0, 0
     for move in moves:
         dx, dy = change[move]
         x += dx
         y += dy
-    if (x, y) not in tiles:
-        tiles[x, y] = True # black
+    if (x, y) not in grid:
+        grid[x, y] = BLACK
     else:
-        tiles[x, y] = not tiles[x, y]
-    for dx, dy in change.values():
-        nx, ny = x + dx, y + dy
-        if (nx, ny) not in tiles:
-            tiles[nx, ny] = False
+        grid[x, y] = not grid[x, y]
 
-p1 = sum(tiles.values())
+p1 = sum(grid.values())
 print(f'Part 1: {p1}')
 
 for day in range(100):
-    new = {}
-    for (x, y), state in tiles.items():
-        adj = 0
-        for (dx, dy) in change.values():
-            nx, ny = x + dx, y + dy
-            neighbor = tiles.get((nx, ny), False)
-            adj += neighbor is True
-
-        assert (x, y) not in new
-        if state is True and (adj == 0 or adj > 2):
-            new[x, y] = False
-        elif state is False and adj == 2:
-            new[x, y] = True
+    surround_all(grid) # ensure all possible spaces that can flip are considered
+    new_grid = {}
+    for (x, y), state in grid.items():
+        adj = sum(grid.get((x + dx, y + dy), WHITE) for (dx, dy) in change.values())
+        if state is BLACK and (adj == 0 or adj > 2):
+            new_grid[x, y] = WHITE
+        elif state is WHITE and adj == 2:
+            new_grid[x, y] = BLACK
         else:
-            new[x, y] = state
+            new_grid[x, y] = state
 
-    for (x, y) in list(new.keys()):
-        for dx, dy in change.values():
-            nx, ny = x + dx, y + dy
-            if (nx, ny) not in new:
-                new[nx, ny] = False
-
-    tiles = new
-    print(f'\rDay {day + 1}: {sum(tiles.values())}', end='')
-
-p2 = sum(tiles.values())
+    grid = new_grid
+    print(f'\rDay {day + 1}: {sum(grid.values())}', end='')
 print()
+
+p2 = sum(grid.values())
 print(f'Part 2: {p2}')
