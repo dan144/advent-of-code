f1fb48e326a49718ef9a3b0f2174f505f219c167
Daniel Gross <dan.12sq@gmail.com>
2020-12-24 01:06:48 -0500

    day 24 cleanup

--- 24.py
+++ 24.py
@@ -7,11 +7,12 @@ test = len(sys.argv) > 1
 input_file = 'input' + sys.argv[0].split('.')[1].lstrip('/') + ('.test' if test else '')
 
 with open(input_file) as f:
-    inp = {line[:-1] for line in f}
+    inp = [line[:-1] for line in f]
 
 WHITE = False
 BLACK = True
 poss_moves = {'e', 'se', 'sw', 'w', 'nw', 'ne'}
+re_moves = re.compile('|'.join(poss_moves))
 
 # moves in a hex grid
 change = {
@@ -24,41 +25,39 @@ change = {
 }
 
 def surround_all(grid):
-    for (x, y) in list(grid.keys()):
+    for x, y in list(grid.keys()):
+        if grid[x, y] is WHITE:
+            # white spaces' neighbors don't need to be added since they won't flip
+            continue
         for dx, dy in change.values():
             nx, ny = x + dx, y + dy
-            if (nx, ny) not in grid:
-                grid[nx, ny] = WHITE
+            grid.setdefault((nx, ny), WHITE)
 
 grid = {}
 for line in inp:
-    moves = re.findall('|'.join(poss_moves), line) # greedy regex
-    x, y = 0, 0
-    for move in moves:
-        dx, dy = change[move]
-        x += dx
-        y += dy
-    if (x, y) not in grid:
+    moves = re_moves.findall(line) # greedy regex
+    x, y = map(sum, zip(*(change[move] for move in moves)))
+
+    # try to flip the tile, set to default BLACK if not present
+    try:
+        grid[x, y] ^= True
+    except KeyError:
         grid[x, y] = BLACK
-    else:
-        grid[x, y] = not grid[x, y]
 
 p1 = sum(grid.values())
 print(f'Part 1: {p1}')
 
 for day in range(100):
     surround_all(grid) # ensure all possible spaces that can flip are considered
-    new_grid = {}
+    flipped_tiles = {}
     for (x, y), state in grid.items():
         adj = sum(grid.get((x + dx, y + dy), WHITE) for (dx, dy) in change.values())
         if state is BLACK and (adj == 0 or adj > 2):
-            new_grid[x, y] = WHITE
+            flipped_tiles[x, y] = WHITE
         elif state is WHITE and adj == 2:
-            new_grid[x, y] = BLACK
-        else:
-            new_grid[x, y] = state
+            flipped_tiles[x, y] = BLACK
 
-    grid = new_grid
+    grid.update(flipped_tiles)
     print(f'\rDay {day + 1}: {sum(grid.values())}', end='')
 print()
 
