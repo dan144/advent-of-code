c0e0c1831c254d26961813059b699cfe0102922a
Daniel Gross <dan.12sq@gmail.com>
2020-12-25 00:27:43 -0500

    day 25 cleanup

--- 25.py
+++ 25.py
@@ -5,42 +5,33 @@ import sys
 test = len(sys.argv) > 1
 input_file = 'input' + sys.argv[0].split('.')[1].lstrip('/') + ('.test' if test else '')
 
-inp = []
 with open(input_file) as f:
-    for line in f:
-        inp.append(int(line.rstrip()))
+    card_pk = int(f.readline().rstrip())
+    door_pk = int(f.readline().rstrip())
 
-card_pk, door_pk = inp
+def transform(val, subject_num):
+    return (val * subject_num) % 20201227
 
-div_by = 20201227
-subject_num = 7
-
-def transform(val):
-    val *= subject_num
-    val = val % div_by
-    return val
-
-def run(val, subject_num, loop_size):
+def calculate(subject_num, loop_size):
+    val = 1
     for i in range(loop_size):
-        val *= subject_num
-        val = val % div_by
+        val = transform(val, subject_num)
     return val
 
-loop_size = 0
+loop_size = 1
 c_val = 1
 d_val = 1
 while True:
-    loop_size += 1
-    c_val = transform(c_val)
-    d_val = transform(d_val)
+    c_val = transform(c_val, 7)
     if c_val == card_pk:
-        print('card')
-        p1 = run(1, door_pk, loop_size)
+        subject_num = door_pk
         break
-    if d_val == door_pk:
-        print('door')
-        p1 = run(1, card_pk, loop_size)
-        break
-print(loop_size)
 
+    d_val = transform(d_val, 7)
+    if d_val == door_pk:
+        subject_num = card_pk
+        break
+    loop_size += 1
+
+p1 = calculate(subject_num, loop_size)
 print(f'Part 1: {p1}')
