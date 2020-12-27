3c87d3ba225f818b67d1085afae2f70bb8f02152
Daniel Gross <dan.12sq@gmail.com>
2020-12-26 21:57:57 -0500

    add script to open files for any day/year (2020+)

---
+++ utilities/edit_day.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -eu
+
+YEAR=$1
+DAY=$(printf "%02d" $2)
+echo $DAY
+
+if [ $YEAR -lt 2015 ] || [ $YEAR -gt 2020 ]; then
+    echo "Year $YEAR not valid"
+    exit 1
+fi
+
+if [ $DAY -lt 1 ] || [ $DAY -gt 25 ]; then
+    echo "Day $DAY not valid"
+    exit 1
+fi
+
+[ $(basename $(pwd)) == "utilities" ] && cd ..
+
+if [ -d $YEAR ]; then
+    mkdir -p $YEAR
+fi
+
+vim -p $YEAR/$DAY.py $YEAR/input$DAY $YEAR/input$DAY.test
