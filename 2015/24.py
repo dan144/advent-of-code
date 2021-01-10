63c6f0ce957f39c0f596ba06ace20d868e3714d5
Daniel Gross <dan.12sq@gmail.com>
2021-01-10 18:06:09 -0500

    2015 day 24 cleanup

--- 2015/24.py
+++ 2015/24.py
@@ -1,16 +1,12 @@
 #!/usr/bin/env python3
 
 import itertools
-import re
 import sys
 
 import utils
 
 test = len(sys.argv) > 1
 input_file = 'input' + sys.argv[0].split('.')[1].lstrip('/') + ('.test' if test else '')
-
-p1 = float('inf')
-p2 = float('inf')
 
 def comp_qe(packages):
     t = 1
@@ -20,27 +16,22 @@ def comp_qe(packages):
 
 with open(input_file) as f:
     nums = sorted(utils.load_num_lines(f), reverse=True)
-
 t_weight = sum(nums)
-g_weight = t_weight // 3
 
-n = 2
-while p1 == float('inf'):
-    for packages in itertools.combinations(nums, n):
-        if sum(packages) == g_weight:
-            qe = comp_qe(packages)
-            p1 = min(p1, qe)
-    n += 1
+def run(n_groups):
+    ans = float('inf')
+    g_weight = t_weight // n_groups
+    n = 2
+    while ans == float('inf'):
+        for packages in itertools.combinations(nums, n):
+            if sum(packages) == g_weight:
+                qe = comp_qe(packages)
+                ans = min(ans, qe)
+        n += 1
+    return ans
 
+p1 = run(3)
 print(f'Part 1: {p1}')
 
-g_weight = t_weight // 4
-n = 2
-while p2 == float('inf'):
-    for packages in itertools.combinations(nums, n):
-        if sum(packages) == g_weight:
-            qe = comp_qe(packages)
-            p2 = min(p2, qe)
-    n += 1
-
+p2 = run(4)
 print(f'Part 2: {p2}')
