d0d6a3fb8b7d8ed98a3c8b1735fe0d721c86870a
Daniel Gross <dan.12sq@gmail.com>
2021-01-13 21:19:43 -0500

    2015 day 22 part 2

--- 2015/22.py
+++ 2015/22.py
@@ -4,7 +4,7 @@ import itertools
 import re
 import sys
 
-from copy import copy
+from copy import copy, deepcopy
 
 import utils
 
@@ -27,9 +27,7 @@ class Fighter:
         self.mana = mana
 
     def stats(self):
-        return (self.hp, self.at, self.mana)
-
-o_boss = Fighter(boss_hp, boss_at, 0)
+        return (self.hp, self.at, self.df, self.mana)
 
 class Spell:
     def __init__(self, cost, turns, at, df, heal, mana):
@@ -43,10 +41,10 @@ class Spell:
     def stats(self):
         return (self.cost, self.turns, self.at, self.df, self.heal, self.mana)
 
-missile = Spell(53, 1, 4, 0, 0, 0)
-drain = Spell(73, 1, 2, 0, 2, 0)
-shield = Spell(113, 6, 0, 7, 0, 0)
-poison = Spell(173, 6, 0, 0, 0, 0)
+missile =  Spell(53,  1, 4, 0, 0, 0)
+drain =    Spell(73,  1, 2, 0, 2, 0)
+shield =   Spell(113, 6, 0, 7, 0, 0)
+poison =   Spell(173, 6, 0, 0, 0, 0)
 recharge = Spell(229, 5, 0, 0, 0, 101)
 
 all_spells = [
@@ -57,86 +55,81 @@ all_spells = [
     missile,
 ]
 
-def fight(player, boss, spellbook, hard_mode):
-    player_go = 0
-    spells = {}
+def fight(player, boss, hard_mode, cost=None, spells=None, next_spell=None):
+    global lowest
+    if spells is None:
+        spells = {}
+    if cost is None:
+        cost = []
+
+    player_go = True
     while player.hp > 0 and boss.hp > 0:
-        if hard_mode and player_go % 2 == 0:
-            # player turn, hard mode
+        if player_go and next_spell is None:
+            # pick the next spell
+            for spell in all_spells:
+                if sum(cost) + spell.cost < lowest: # cheap enough to try
+                    result, t_cost = fight(copy(player), copy(boss), hard_mode, cost=deepcopy(cost), spells=deepcopy(spells), next_spell=spell)
+                    if result and t_cost < lowest:
+                        lowest = t_cost
+                        print(f' Player wins: {lowest}   \r', end='')
+            return True, lowest
+
+        if hard_mode and player_go:
             player.hp -= 1
             if player.hp <= 0:
-                return player, boss, spellbook
-        #print('Turn', player_go, 'player' if player_go % 2 == 0 else 'boss')
-        #print('Player', player.hp, sum([spell.df for spell in spells.values()]), player.mana)
-        #print('Boss', boss.hp)
+                return False, sum(cost)
 
-        for spell in spells.values():
+        if spells.get(173): # poison
+            boss.hp -= 3
+        if boss.hp <= 0:
+            return True, sum(cost)
+
+        if spells.get(229): # recharge
+            player.mana += spells[229].mana
+
+        for s_cost, spell in copy(list(spells.items())):
             spell.turns -= 1
-            if spell.cost == 173: # poison
-                boss.hp -= 3
-            if spell.cost == 229: # recharge
-                player.mana += spell.mana
-            if boss.hp <= 0:
-                return player, boss, spellbook
+            if spell.turns == 0:
+                player.at -= spell.at
+                player.df -= spell.df
+                spells.pop(s_cost)
 
-        if player_go % 2 == 0:
-            spell = copy(spellbook.pop(0))
-            if spell.cost in spells.keys():
-                return player, boss, spellbook
-            if spell.cost > player.mana:
-                return player, boss, spellbook
-            player.mana -= spell.cost
-            player.at += spell.at
-            player.df += spell.df
+        if player_go:
+            if next_spell.cost in spells.keys() and spells[next_spell.cost].turns > 0:
+                return False, sum(cost)
 
-            spells[spell.cost] = copy(spell)
-            if spell.cost == 73:
+            player.mana -= next_spell.cost
+            if player.mana < 0:
+                return False, sum(cost)
+            cost.append(next_spell.cost)
+
+            player.at += next_spell.at
+            player.df += next_spell.df
+
+            spells[next_spell.cost] = copy(next_spell)
+            if next_spell.cost == 73:
                 player.hp += 2
+            next_spell = None
 
             boss.hp -= player.at
         else:
             player.hp -= max(1, boss.at - player.df)
-        player_go += 1
+        player_go = not player_go
 
-        for spell in copy(list(spells.values())):
-            if spell.turns == 0:
-                player.at -= spell.at
-                player.df -= spell.df
-                spells.pop(spell.cost)
-    return player, boss, spellbook
-
-def run(hard, n):
-    ans = 1295 #float('inf')
-    while ans >= 1295:
-        print('Spellbooks of length', n)
-        x = 0
-        print(len(all_spells)**n)
-        for spellbook in itertools.product(all_spells, repeat=n):
-            x += 1
-            print(f'\r{x}', end='')
-            player = Fighter(player_hp, 0, player_mana)
-            cost = sum([spell.cost for spell in spellbook])
-            if cost >= ans:
-                continue
-            try:
-                player, boss, leftovers = fight(player, copy(o_boss), list(spellbook), hard)
-            except IndexError:
-                pass #print('Need more spells!')
-            else:
-                if boss.hp <= 0:
-                    print([spell.cost for spell in spellbook])
-                    cost -= sum([spell.cost for spell in leftovers])
-                    print('Player wins:', cost)
-                    ans = min(ans, cost)
-        n += 1
-    return ans
+    return player.hp > 0, sum(cost)
 
 player_hp = 10 if test else 50
 player_mana = 250 if test else 500
 
-p1 = run(False, 9)
+o_player = Fighter(player_hp, 0, player_mana)
+o_boss = Fighter(boss_hp, boss_at, 0)
+
+lowest = float('inf')
+_, p1 = fight(copy(o_player), copy(o_boss), False)
+print()
 print(f'Part 1: {p1}')
-p2 = run(True, 12)
-# below 1362 (from 10)
-# below 1295 (from 11)
+
+lowest = float('inf')
+_, p2 = fight(copy(o_player), copy(o_boss), True)
+print()
 print(f'Part 2: {p2}')
