b704e99c59ba077f630f8c6db0b0b9e7ac66314b
Daniel Gross <dan.12sq@gmail.com>
2021-01-26 10:02:10 -0500

    2017 day 17 part 2

--- 2017/17.py
+++ 2017/17.py
@@ -6,6 +6,8 @@ import sys
 
 import utils
 
+from blist import blist
+
 test = len(sys.argv) > 1
 input_file = 'input' + sys.argv[0].split('.')[1].lstrip('/') + ('.test' if test else '')
 
@@ -16,7 +18,7 @@ with open(input_file) as f:
     steps = int(f.readline().rstrip())
 
 pos = 0
-nums = [0]
+nums = blist([0])
 for i in range(1, 2017 + 1):
     pos = (pos + steps) % len(nums) + 1
     nums.insert(pos, i)
@@ -25,7 +27,7 @@ idx = nums.index(2017)
 p1 = nums[(idx + 1) % len(nums)]
 print(f'Part 1: {p1}')
 
-for i in range(i, 50000000 + 1):
+for i in range(i + 1, 50000000 + 1):
     if i % 100000 == 0:
         print(f'\r{i}', end='')
     pos = (pos + steps) % len(nums) + 1
