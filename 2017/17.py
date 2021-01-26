a1951d79775d309f0ced0045f11e847eea7a0def
Daniel Gross <dan.12sq@gmail.com>
2021-01-26 09:39:07 -0500

    2017 day 17 part 1

---
+++ 2017/17.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import itertools
+import re
+import sys
+
+import utils
+
+test = len(sys.argv) > 1
+input_file = 'input' + sys.argv[0].split('.')[1].lstrip('/') + ('.test' if test else '')
+
+p1 = 0
+p2 = 0
+
+with open(input_file) as f:
+    steps = int(f.readline().rstrip())
+
+pos = 0
+nums = [0]
+for i in range(1, 2017 + 1):
+    pos = (pos + steps) % len(nums) + 1
+    nums.insert(pos, i)
+
+idx = nums.index(2017)
+p1 = nums[(idx + 1) % len(nums)]
+print(f'Part 1: {p1}')
+
+for i in range(i, 50000000 + 1):
+    if i % 100000 == 0:
+        print(f'\r{i}', end='')
+    pos = (pos + steps) % len(nums) + 1
+    nums.insert(pos, i)
+
+idx = nums.index(0)
+p2 = nums[(idx + 1) % len(nums)]
+print(f'\nPart 2: {p2}')
