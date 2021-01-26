bac42a7d750e500cc4fa5d34d3f86d9552897940
Daniel Gross <dan.12sq@gmail.com>
2021-01-26 00:11:13 -0500

    2017 day 15 part 2

--- 2017/15.py
+++ 2017/15.py
@@ -23,32 +23,41 @@ a_factor = 16807
 b_factor = 48271
 div = 2147483647
 
-from_a = []
-from_b = []
-p = 5000001
-n = 0
-while n <= 40000000 or p <= 5000000:
+for n in range(40000000):
     if n % 100000 == 0:
-        print(f'\r{n} {p}', end='')
+        print(f'\r{n}', end='')
     gen_a = (gen_a * a_factor) % div
     gen_b = (gen_b * b_factor) % div
-    if n <= 40000000:
-        if gen_a % 2**16 == gen_b % 2**16:
-            p1 += 1
-        if n == 40000000:
-            print(f'\nPart 1: {p1}')
+    if gen_a % 2**16 == gen_b % 2**16:
+        p1 += 1
+print(f'\nPart 1: {p1}')
 
-    #if gen_a % 4 == 0:
-    #    from_a.append(gen_a)
-    #if gen_b % 8 == 0:
-    #    from_b.append(gen_b)
-
-    #if from_a and from_b:
-    #    p += 1
-    #    a = from_a.pop(0)
-    #    b = from_b.pop(0)
-    #    if a % 2**16 == b % 2**16:
-    #        p2 += 1
+from_a = None
+from_b = None
+compute_a = True
+compute_b = True
+p = 0
+n = 0
+while p < 5000000:
     n += 1
+    if p % 10000 == 0:
+        print(f'\r{p}', end='')
+
+    if from_a is None:
+        gen_a = (gen_a * a_factor) % div
+    if from_b is None:
+        gen_b = (gen_b * b_factor) % div
+
+    if gen_a % 4 == 0:
+        from_a = gen_a
+    if gen_b % 8 == 0:
+        from_b = gen_b
+
+    if from_a and from_b:
+        p += 1
+        if from_a % 2**16 == from_b % 2**16:
+            p2 += 1
+        from_a = None
+        from_b = None
 
 print(f'\nPart 2: {p2}')
