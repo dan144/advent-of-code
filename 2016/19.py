a592b02c821987ea90d4c83a27b3789b0ffea9c9
Daniel Gross <dan.12sq@gmail.com>
2021-01-31 02:01:57 -0500

    2016 day 19 part 2

--- 2016/19.py
+++ 2016/19.py
@@ -3,6 +3,8 @@
 import re
 import sys
 
+from blist import blist
+
 test = len(sys.argv) > 1
 input_file = 'input' + sys.argv[0].split('.')[1].lstrip('/') + ('.test' if test else '')
 
@@ -14,7 +16,7 @@ with open(input_file) as f:
 
 elves = list(range(num))
 while len(elves) != 1:
-    print(len(elves))
+    print(f'\r{len(elves)}', end=' ')
     remove = set()
     for i in range(0, len(elves), 2):
         try:
@@ -24,24 +26,16 @@ while len(elves) != 1:
     elves = list(sorted(set(elves) - remove))
 
 p1 = elves.pop() + 1
-print(f'Part 1: {p1}')
+print(f'\rPart 1: {p1}')
 
-elves = {x + 1 for x in range(num)}
-i = 0
+elves = blist(range(1, num + 1))
 while len(elves) != 1:
     if len(elves) % 1000 == 0:
-        print(len(elves))
+        print(f'\r{len(elves)}', end=' ')
 
-    n_other = (len(elves) - 1) / 2
-    n_other = int(n_other) + (1 if n_other % 1 != 0 else 0)
-    n_other = (n_other + i) % len(elves)
-
-    other = sorted(elves)[n_other]
-    elves.remove(other)
-
-    i += 1
-    if i > len(elves):
-        i = 0
+    n_other = len(elves) // 2
+    elves.pop(n_other)
+    elves = elves[1:] + [elves[0]]
 
 p2 = elves.pop()
-print(f'Part 2: {p2}')
+print(f'\rPart 2: {p2}')
