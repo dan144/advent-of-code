f8ba01d119b4dcd0c6d3395f8de95bbd8b6e5bc6
Daniel Gross <dan.12sq@gmail.com>
2021-12-03 00:17:44 -0500

    2021 day 3 cleanup

--- 2021/03.py
+++ 2021/03.py
@@ -13,7 +13,6 @@ import utils
 test = len(sys.argv) > 1
 input_file = 'input' + sys.argv[0].split('.')[1].lstrip('/') + ('.test' if test else '')
 
-p1 = 0
 p2 = 1
 
 inp = []
@@ -22,46 +21,36 @@ with open(input_file) as f:
         inp.append(line.strip())
 
 # Part 1
-z, o = [0] * len(inp[0]), [0] * len(inp[0])
+counts = [0] * len(inp[0])
+
 for num in inp:
     for i, bit in enumerate(num):
-        if bit == '0':
-            z[i] += 1
-        else:
-            o[i] += 1
+        # keep a count of 1s minus count of 0s for each position
+        counts[i] += 1 if bit == '1' else -1
 
-g = ''
-e = ''
-for i in range(len(z)):
-    if z[i] > o[i]:
-        g += '0'
-        e += '1'
-    else:
-        g += '1'
-        e += '0'
+# for each value in counts, map a cast of the str value of the more common digit, then join
+g = ''.join((map(lambda x: '1' if x > 0 else '0', counts)))
+e = ''.join((map(lambda x: '0' if x > 0 else '1', counts)))
 
 p1 = int(g, 2) * int(e, 2)
 print(f'Part 1: {p1}')
 
 # Part 2
 for t in range(2):
-    nums = set(inp)
+    nums = set(inp) # order doesn't matter and set doesn't need an import like copy
     i = 0
     while len(nums) > 1:
-        z, o = 0, 0
+        o_less_z = 0
         for num in nums:
-            if num[i] == '0':
-                z += 1
-            else:
-                o += 1
+            o_less_z += 1 if num[i] == '1' else -1
 
         if t == 0: # oxygen, most common
-            d = '1' if o >= z else '0'
+            d = '1' if o_less_z >= 0 else '0'
         else: # CO2, least common
-            d = '0' if o >= z else '1'
+            d = '0' if o_less_z >= 0 else '1'
 
         nums = set(filter(lambda x: x[i] == d, nums))
         i += 1
-    p2 *= int(nums.pop(), 2)
+    p2 *= int(nums.pop(), 2) # the last num is one of the two desired values, just multiply them
 
 print(f'Part 2: {p2}')
