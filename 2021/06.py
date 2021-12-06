5ae5994722fc3da6d4e12763dfa9c3ab6e640e5b
Daniel Gross <dan.12sq@gmail.com>
2021-12-06 00:32:27 -0500

    2021 day 6 cleanup: var name

--- 2021/06.py
+++ 2021/06.py
@@ -11,20 +11,20 @@ inp = []
 with open(input_file) as f:
     inp = utils.load_comma_sep_nums(f) # 1,2,3,...
 
-nums = {}
+timer = {}
 for v in inp:
-    nums[v] = nums.get(v, 0) + 1
+    timer[v] = timer.get(v, 0) + 1
 
 for day in range(256):
-    new = nums.get(0, 0)
+    new = timer.get(0, 0)
     for l in range(8):
-        nums[l] = nums.get(l+1, 0)
-    nums[8] = new
-    nums[6] += new
-    #print(sorted(nums.items(), key=lambda x: x[0]))
+        timer[l] = timer.get(l+1, 0)
+    timer[8] = new
+    timer[6] += new
+    #print(sorted(timer.items(), key=lambda x: x[0]))
     if day == 79:
-        p1 = sum(nums.values())
-p2 = sum(nums.values())
+        p1 = sum(timer.values())
+p2 = sum(timer.values())
 
 print(f'Part 1: {p1}')
 print(f'Part 2: {p2}')
