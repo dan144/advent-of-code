b0c06b93d76aa0587cd5350daea80d814ecfac2e
Daniel Gross <dan.12sq@gmail.com>
2021-12-08 00:47:04 -0500

    2021 day 8 cleanup

--- 2021/08.py
+++ 2021/08.py
@@ -1,16 +1,9 @@
 #!/usr/bin/env python3
 
 import itertools
-import re
 import sys
 
 import utils
-### available functions:
-# get_grid_edges: min_x, min_y, max_x, max_y
-# display_grid
-# find_dist(grid, 0, (x,y) start, (x,y) dest) - open=True, wall=False
-# manh(p1[, p2]) - n-dim Manhattan dist; omit p2 for dist from origin
-# is_prime
 
 test = len(sys.argv) > 1
 input_file = 'input' + sys.argv[0].split('.')[1].lstrip('/') + ('.test' if test else '')
@@ -24,104 +17,86 @@ with open(input_file) as f:
 
 # Part 1
 segs = []
-seen = set()
 for line in inp:
-    segs.append(([], []))
-    pipe = False
+    segs.append([])
+    pipe = False # only monitor things after |
     for v in line:
         if v == '|':
             pipe = True
         else:
-            if pipe:
-                seen.add(v)
-                segs[-1][1].append(v)
-                if len(v) in {2, 4, 3, 7}:
-                    p1 += 1
-            else:
-                segs[-1][0].append(v)
+            if pipe and len(v) in {2, 4, 3, 7}:
+                p1 += 1
+            segs[-1].append(v)
 print(f'Part 1: {p1}')
 
 # Part 2
 
-# 2 5
-# 3 5
-# 5 5
-
-# 0 6
-# 6 6
-# 9 6
-
-# 1 2
-# 4 4
-# 7 3
-# 8 7
-
 for line in segs:
-    for p in itertools.permutations('abcdefg'):
+    for p in itertools.permutations('abcdefg'): # try all permutations of segments
         valid = True
         guesses = {}
-        for num in line[0] + line[1]:
-            if len(num) == 2: # 1
+        for num in line:
+            num_l = len(num)
+            num_s = set(num)
+
+            if num_l == 2: # 1
                 g = {p[2], p[5]}
-                if g != set(num):
+                if g != num_s:
                     valid = False
-                #elif guesses.get(1) != g:
-                    pass #valid = False
                 else:
                     guesses[1] = g
-            elif len(num) == 3: # 7
+            elif num_l == 3: # 7
                 g = {p[0], p[2], p[5]}
-                if g != set(num):
+                if g != num_s:
                     valid = False
-                #elif guesses.get(7) != g:
-                    pass #valid = False
                 else:
                     guesses[7] = g
-            elif len(num) == 4: # 4
+            elif num_l == 4: # 4
                 g = {p[x] for x in (1, 2, 3, 5)}
-                if g != set(num):
+                if g != num_s:
                     valid = False
-                #elif guesses.get(4) != g:
-                    pass #valid = False
                 else:
                     guesses[4] = g
-            elif len(num) == 7: # 8
-                guesses[8] = set(num)
-            elif len(num) == 5: # 2,3,5
+            elif num_l == 7: # 8
+                guesses[8] = num_s
+            elif num_l == 5: # 2,3,5
                 g2 = {p[x] for x in (0, 2, 3, 4, 6)}
                 g3 = {p[x] for x in (0, 2, 3, 5, 6)}
                 g5 = {p[x] for x in (0, 1, 3, 5, 6)}
-                if g2 == set(num): # 2
+                if g2 == num_s: # 2
                     guesses[2] = g2
-                elif g3 == set(num): # 3
+                elif g3 == num_s: # 3
                     guesses[3] = g3
-                elif g5 == set(num):
+                elif g5 == num_s: # 5
                     guesses[5] = g5
                 else:
                     valid = False
-            elif len(num) == 6: # 0,6,9
+            elif num_l == 6: # 0,6,9
                 g0 = {p[x] for x in (0, 1, 2, 4, 5, 6)}
                 g6 = {p[x] for x in (0, 1, 3, 4, 5, 6)}
                 g9 = {p[x] for x in (0, 1, 2, 3, 5, 6)}
-                if g0 == set(num): # 0
+                if g0 == num_s: # 0
                     guesses[0] = g0
-                elif g6 == set(num): # 6
+                elif g6 == num_s: # 6
                     guesses[6] = g6
-                elif g9 == set(num): # 9
+                elif g9 == num_s: # 9
                     guesses[9] = g9
                 else:
                     valid = False
-            if valid == False:
+
+            if not valid:
                 break
+
         if len(guesses.keys()) != 10:
-            continue # wrong
+            continue # didn't match all ten, so must be wrong
 
         output = 0
-        for num in line[1]:
+        for num in line[-4:]: # sum up the values of the last 4
             output *= 10
             for k, v in guesses.items():
                 if v == set(num):
                     output += k
         p2 += output
+        break
 
 print(f'Part 2: {p2}')
