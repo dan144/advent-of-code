425fa7bc7a15c6f8f583c34a0a060d6af14e6ba7
Daniel Gross <dan.12sq@gmail.com>
2021-12-12 01:37:19 -0500

    2021 day 12 cleanup

--- 2021/12.py
+++ 2021/12.py
@@ -1,28 +1,12 @@
 #!/usr/bin/env python3
 
 from copy import deepcopy
+
 import string
-import itertools
-import re
 import sys
 
-import utils
-### available functions:
-# get_grid_edges: min_x, min_y, max_x, max_y
-# display_grid
-# find_dist(grid, 0, (x,y) start, (x,y) dest) - open=True, wall=False
-# manh(p1[, p2]) - n-dim Manhattan dist; omit p2 for dist from origin
-# is_prime
-# adjs - set of dx,dy values for LRUD adjacencies
-# diags - set of dx,dy values for diagonals
-# all_dirs set of dx,dy values for all 8 surrounding values
-
 test = len(sys.argv) > 1
-test = False
 input_file = 'input' + sys.argv[0].split('.')[1].lstrip('/') + ('.test' if test else '')
-
-p1 = 0
-p2 = 0
 
 inp = []
 with open(input_file) as f:
@@ -97,7 +81,7 @@ print(f'Part 1: {p1}')
 
 paths = set()
 
-# number of paths when visiting the key node up to twice
+# For my input: number of paths when visiting the key node up to twice
 s = {
     'yq': 18003,
     'oe': 19462,
@@ -115,13 +99,15 @@ s = {
 # 
 # Originally computed this by running a copy of this that only doubled a single thread from a cli arg
 # Essentially a threaded solution but without actually using threading
-p2 = sum(s.values()) - (3000 * len(s.values())) + 3000
-print('Part 2:', p2)
-print('(Not computed above; computing now)')
+if not test:
+    p2 = sum(s.values()) - (3000 * len(s.values())) + 3000
+    print('Part 2:', p2)
+    print('(Hardcoded for my input; computing on input file now)')
 
-# This solution works, just really slowly
+# This solution works, just really slowly for actual inputs
 for x in small:
-    print(x)
+    if not test:
+        print(x)
     checked = set()
     can_double = x
     step(['start'], routes, False)
