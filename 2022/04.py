a3ece509008a9387acc18cfbd2ac3c60353c7ff7
Daniel Gross <dan.12sq@gmail.com>
2022-12-04 00:15:36 -0500

    2022 day 4 cleanup

--- 2022/04.py
+++ 2022/04.py
@@ -1,21 +1,7 @@
 #!/usr/bin/env python3
 
-import itertools
 import re
 import sys
-
-import utils
-### available functions:
-# get_grid_edges - min_x, min_y, max_x, max_y
-# display_grid((y, x) grid) - display values in 2D map grid
-# find_dist(grid, 0, (x,y) start, (x,y) dest) - open=True, wall=False
-# find_cheapest(grid, (y,x) start, (y,x) end) - grid of ints, finds cheapest path from start to end, returns cost dist
-# transpose_grid(grid) - swap key values from (x, y) to (y, x) and back
-# manh(p1[, p2]) - n-dim Manhattan dist; omit p2 for dist from origin
-# is_prime
-# adjs - set of dx,dy values for LRUD adjacencies
-# diags - set of dx,dy values for diagonals
-# all_dirs set of dx,dy values for all 8 surrounding values
 
 test = len(sys.argv) > 1
 input_file = 'input' + sys.argv[0].split('.')[1].lstrip('/') + ('.test' if test else '')
@@ -23,32 +9,16 @@ input_file = 'input' + sys.argv[0].split('.')[1].lstrip('/') + ('.test' if test 
 p1 = 0
 p2 = 0
 
-inp = []
 with open(input_file) as f:
     for line in f:
-        inp.append(line.strip())
+        # for each x-y in line, create a set of that range of numbers
+        a, b = map(lambda x: set(range(int(x[0]), int(x[1])+1)), re.findall(r'([0-9]+)-([0-9]+)', line))
+        shared = a & b
 
-# Part 1
+        if shared == b or shared == a: # shared numbers equal one of the whole sets
+            p1 += 1
+        if shared:
+            p2 += 1
 
-for line in inp:
-    a, b = line.split(',')
-    a1, a2 = map(int, a.split('-'))
-    b1, b2 = map(int, b.split('-'))
-
-    if a1 <= b1 and b2 <= a2:
-        p1 += 1
-    elif b1 <= a1 and a2 <= b2:
-        p1 += 1
 print(f'Part 1: {p1}')
-
-# Part 2
-
-for line in inp:
-    a, b = line.split(',')
-    a1, a2 = map(int, a.split('-'))
-    b1, b2 = map(int, b.split('-'))
-    a = set(range(a1, a2+1))
-    b = set(range(b1, b2+1))
-    if a & b:
-        p2 += 1
 print(f'Part 2: {p2}')
