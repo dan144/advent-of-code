9e3b4dae021192837580fa0cd4247f3de293755c
Daniel Gross <dan.12sq@gmail.com>
2022-12-07 16:43:34 +0000

    2022 day 7 cleanup

--- 2022/07.py
+++ 2022/07.py
@@ -1,41 +1,29 @@
 #!/usr/bin/env python3
 
-import json
 import sys
 
 test = len(sys.argv) > 1
 input_file = 'input' + sys.argv[0].split('.')[1].lstrip('/') + ('.test' if test else '')
 
 p1 = 0
-p2 = 0
 
-inp = []
-with open(input_file) as f:
-    for line in f:
-        inp.append(line.strip())
-
-# Part 1
 files = {}
 mydir = ['/']
 alldirs = set()
-printing = False
-for cmd in inp:
-    cmd = cmd.split()
-    if cmd[0] == '$': # is a command
-        if printing: # done reading output
-            printing = False
-        if cmd[1] == 'cd':
-            if cmd[2] == '/':
-                mydir = ['/']
-            elif cmd[2] == '..':
-                mydir.pop()
-            elif cmd[2] != '.':
-                mydir.append(cmd[2])
-        elif cmd[1] == 'ls': # print a dir
-            printing = True
-            alldirs.add(tuple(mydir))
-    else:
-        if printing:
+with open(input_file) as f:
+    for line in f:
+        cmd = line.strip().split()
+        if cmd[0] == '$': # is a command
+            if cmd[1] == 'cd':
+                if cmd[2] == '/':
+                    mydir = ['/']
+                elif cmd[2] == '..':
+                    mydir.pop()
+                elif cmd[2] != '.':
+                    mydir.append(cmd[2])
+            elif cmd[1] == 'ls': # print a dir
+                alldirs.add(tuple(mydir))
+        else: # is an ls output
             f = files
             for d in mydir:
                 if d not in f:
@@ -43,10 +31,9 @@ for cmd in inp:
                 f = f[d]
             if cmd[0] != 'dir':
                 f[cmd[1]] = int(cmd[0])
-        else:
-            assert False
 
 def get_size(files, dirs):
+    # return the size of a dir, including subdirs
     f = files
     for d in dirs:
         f = f[d]
@@ -59,6 +46,7 @@ def get_size(files, dirs):
             size += v
     return size
 
+# Part 1
 dir_sizes = {}
 for d in alldirs:
     size = get_size(files, d)
@@ -71,9 +59,9 @@ print(f'Part 1: {p1}')
 # Part 2
 free_space = 70000000 - get_size(files, ('/',))
 needed_space = 30000000 - free_space
+# equivalent: needed_space = get_size(files, ('/',)) - 40000000
 
 for k, v in sorted(dir_sizes.items(), key=lambda x: x[1]):
     if v >= needed_space:
-        p2 = v
+        print(f'Part 2: {v}')
         break
-print(f'Part 2: {p2}')
