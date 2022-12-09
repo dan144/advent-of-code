491aa1e11793fb2f783f3c9a3865d43d8f7a36af
Daniel Gross <dan.12sq@gmail.com>
2022-12-09 09:09:18 +0000

    2022 day 9 part 2

--- 2022/09.py
+++ 2022/09.py
@@ -30,56 +30,62 @@ with open(input_file) as f:
 
 # Part 1
 
-head = [0, 0]
-tail = [0, 0]
-tail_pos = {(0, 0)}
-for line in inp:
-    d, dist = line.split()
-    dist = int(dist)
-    for step in range(dist):
-        # move head
-        if d == 'R':
-            head[0] += 1
-        elif d == 'L':
-            head[0] -= 1
-        elif d == 'U':
-            head[1] += 1
-        elif d == 'D':
-            head[1] -= 1
-        else:
-            assert False
+def run(n_tails):
+    head = [0, 0]
+    tails = []
+    for _ in range(n_tails):
+        tails.append([0, 0])
+    tail_pos = {(0, 0)}
+    for line in inp:
+        d, dist = line.split()
+        dist = int(dist)
+        for step in range(dist):
+            # move head
+            if d == 'R':
+                head[0] += 1
+            elif d == 'L':
+                head[0] -= 1
+            elif d == 'U':
+                head[1] += 1
+            elif d == 'D':
+                head[1] -= 1
+            else:
+                assert False
+    
+            # move tail(s)?
+            for i, tail in enumerate(tails):
+                following = head if i == 0 else tails[i - 1]
+                dx = tail[0] - following[0]
+                dy = tail[1] - following[1]
+                if abs(dx) > 1 or abs(dy) > 1:
+                    if dx != 0:
+                        tail[0] -= 1 if (dx > 0) else -1
+                    if dy != 0:
+                        tail[1] -= 1 if (dy > 0) else -1
+                #if dx > 1:
+                #    tail[0] -= 1
+                #elif dx < -1:
+                #    tail[0] += 1
+                #if dy > 1:
+                #    tail[1] -= 1
+                #elif dy < -1:
+                #    tail[1] += 1
+    
+                tail_pos.add(tuple(tails[-1]))
+    
+            #grid = {}
+            #for pos in tail_pos:
+            #    grid[pos] = '#'
+            #grid[tuple(head)] = 'H'
+            #grid[tuple(tail)] = 'T'
+            #grid = utils.transpose_grid(grid)
+            #utils.display_grid(grid)
+            #input()
+    return len(tail_pos)
 
-        # move tail?
-        dx = tail[0] - head[0]
-        dy = tail[1] - head[1]
-        if abs(dx) > 1 or abs(dy) > 1:
-            if dx != 0:
-                tail[0] -= 1 if (dx > 0) else -1
-            if dy != 0:
-                tail[1] -= 1 if (dy > 0) else -1
-        #if dx > 1:
-        #    tail[0] -= 1
-        #elif dx < -1:
-        #    tail[0] += 1
-        #if dy > 1:
-        #    tail[1] -= 1
-        #elif dy < -1:
-        #    tail[1] += 1
-
-        tail_pos.add(tuple(tail))
-
-        #grid = {}
-        #for pos in tail_pos:
-        #    grid[pos] = '#'
-        #grid[tuple(head)] = 'H'
-        #grid[tuple(tail)] = 'T'
-        #grid = utils.transpose_grid(grid)
-        #utils.display_grid(grid)
-        #input()
-
-p1 = len(tail_pos)
+p1 = run(1)
 print(f'Part 1: {p1}')
 
 # Part 2
-
+p2 = run(9)
 print(f'Part 2: {p2}')
