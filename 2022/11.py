6d23c4946b2448dd78ec4799d19edead4ac73863
Daniel Gross <dan.12sq@gmail.com>
2022-12-11 16:55:54 +0100

    2022 day 11 part 2

--- 2022/11.py
+++ 2022/11.py
@@ -12,6 +12,7 @@ p2 = 0
 
 monkeys = {}
 business = {}
+m = 1
 with open(input_file) as f:
     for line in f:
         line = line.strip().split()
@@ -33,42 +34,29 @@ with open(input_file) as f:
             monkeys[monkey]['op'] = ' '.join(line[3:])
         elif line[0] == 'Test:':
             monkeys[monkey]['test'] = int(line[-1])
+            m *= int(line[-1])
         elif line[1] == 'true:':
             monkeys[monkey]['true'] = int(line[-1])
         elif line[1] == 'false:':
             monkeys[monkey]['false'] = int(line[-1])
 
 # Part 1
-for monkey in monkeys.keys():
-    pass #print(monkey)
-
-for r in range(20):
-    print(r)
-    new_monkeys = deepcopy(monkeys)
-    for monkey in new_monkeys.keys():
-        new_monkeys[monkey]['items'] = []
-
+for r in range(10000):
     monkey_list = monkeys.keys()
     for monkey in monkey_list:
         value = monkeys[monkey]
         for item in value['items']:
             business[monkey] += 1
-            #print(item)
             worry = eval(value['op'].replace('old', str(item)))
-            #print(worry)
-            worry //= 3
-            #print(worry)
+            #worry //= 3
+            worry = worry % m
             if worry % value['test'] == 0:
                 give_to = value['true']
             else:
                 give_to = value['false']
-            #print(give_to)
             monkeys[give_to]['items'].append(worry)
         monkeys[monkey]['items'] = []
-            #input()
-    #monkeys = new_monkeys
 
-print(business)
 vals = sorted(business.values(), reverse=True)
 p1 = vals[0] * vals[1]
 print(f'Part 1: {p1}')
