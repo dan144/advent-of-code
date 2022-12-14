a79bb9d3666f9ad1ad7dba08d2a3835e51aef729
Daniel Gross <dan.12sq@gmail.com>
2022-12-14 12:02:48 -0500

    2022 day 13 part 2

--- 2022/13.py
+++ 2022/13.py
@@ -8,7 +8,7 @@ test = len(sys.argv) > 1
 input_file = 'input' + sys.argv[0].split('.')[1].lstrip('/') + ('.test' if test else '')
 
 p1 = 0
-p2 = 0
+p2 = 1
 
 lists = []
 with open(input_file) as f:
@@ -22,11 +22,7 @@ with open(input_file) as f:
             s = []
 lists.append(s)
 
-# Part 1
-
 def compare(a, b):
-    if idx == 1:
-        print(a, b)
     if isinstance(a, int) and isinstance(b, int):
         if a < b:
             return True
@@ -54,16 +50,37 @@ def compare(a, b):
 for idx, (a, b) in enumerate(lists):
     c = compare(a, b)
     if c is True:
-        print('Yes')
-        print(idx + 1)
         p1 += idx + 1
-    elif c is False:
-        print('No')
     elif c is None:
         assert False
 
+# Part 1
 print(f'Part 1: {p1}')
 
 # Part 2
 
+all_lists = [[[2]], [[6]]]
+for a, b in lists:
+    all_lists.extend([a, b])
+
+sorted_lists = []
+while all_lists:
+    for a in all_lists:
+        a_at_end = True
+        for b in all_lists:
+            if a == b:
+                continue
+            c = compare(a, b)
+            if c is True: # is not at the end
+                a_at_end = False
+                break
+        if a_at_end is True:
+            break
+    if a_at_end is True:
+        sorted_lists = [a] + sorted_lists
+        all_lists.remove(a)
+
+for idx, a in enumerate(sorted_lists):
+    if a == [[2]] or a == [[6]]:
+        p2 *= idx + 1
 print(f'Part 2: {p2}')
