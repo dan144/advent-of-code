2d9b756442c4dc6c702480d3b72792125c17749a
Daniel Gross <dan.12sq@gmail.com>
2022-12-15 11:34:27 -0500

    2022 day 15 cleanup: run both parts, converge faster

--- 2022/15.py
+++ 2022/15.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-import itertools
 import re
 import sys
 
@@ -8,14 +7,8 @@ import utils
 ### available functions:
 # get_grid_edges - min_x, min_y, max_x, max_y
 # display_grid((y, x) grid) - display values in 2D map grid
-# find_dist(grid, 0, (x,y) start, (x,y) dest) - open=True, wall=False
-# find_cheapest(grid, (y,x) start, (y,x) end) - grid of ints, finds cheapest path from start to end, returns cost dist
-# transpose_grid(grid) - swap key values from (x, y) to (y, x) and back
 # manh(p1[, p2]) - n-dim Manhattan dist; omit p2 for dist from origin
-# is_prime
-# adjs - set of dx,dy values for LRUD adjacencies
 # diags - set of dx,dy values for diagonals
-# all_dirs set of dx,dy values for all 8 surrounding values
 
 test = len(sys.argv) > 1
 input_file = 'input' + sys.argv[0].split('.')[1].lstrip('/') + ('.test' if test else '')
@@ -23,55 +16,55 @@ input_file = 'input' + sys.argv[0].split('.')[1].lstrip('/') + ('.test' if test 
 p1 = 0
 p2 = 0
 
-grid = {}
-pairs = []
-specials = set()
+grid = {} # create grid to get x bounds
+pairs = [] # maintain sensor-beacon pairs with Manhattan dist
+specials = set() # list of all places that known things are
 with open(input_file) as f:
     for line in f:
         sx, sy, bx, by = map(int, re.findall(r'-?\d+', line))
         d = utils.manh((sx, sy), (bx, by))
+
         pairs.append(((sx, sy), (bx, by), d))
-        grid[sy, sx] = 'S'
-        grid[by, bx] = 'B'
         specials.add((sx, sy))
         specials.add((bx, by))
 
+        grid[sy, sx] = 'S'
+        grid[by, bx] = 'B'
+
+        # notes the cardinal direction bounds on the grid
+        grid[sy + d, sx] = 'v'
+        grid[sy - d, sx] = '^'
+        grid[sy, sx + d] = '>'
+        grid[sy, sx - d] = '<'
+
 # Part 1
 
-for s, b, d in pairs:
-    sx, sy = s
-    bx, by = b
-    #d = utils.manh(s, b)
-    grid[sy + d, sx] = '.'
-    grid[sy - d, sx] = '.'
-    grid[sy, sx + d] = '.'
-    grid[sy, sx - d] = '.'
-
 min_x, min_y, max_x, max_y = utils.get_grid_edges(grid)
-print(min_x, min_y, max_x, max_y)
 if test:
     utils.display_grid(grid)
 
-#y = 10 if test else 2000000
-#for x in range(min_x, max_x + 1):
-#    for s, b, d in pairs:
-#        #d = utils.manh(s, b)
-#        if utils.manh(s, (x, y)) <= d and (x, y) not in specials:
-#            p1 += 1
-#            if test:
-#                print(p1, (x, y))
-#            else:
-#                print(f'\r{p1}  ', end='')
-#            break
-#
-#print(f'Part 1: {p1}')
+y = 10 if test else 2000000
+no_change = 0
+for x in range(min_x, max_x + 1):
+    no_change += 1
+    for s, b, d in pairs:
+        if utils.manh(s, (x, y)) <= d and (x, y) not in specials:
+            p1 += 1
+            no_change = 0
+            print(f'\r{p1}  ', end='')
+            break
+    if p1 > 0 and no_change > 1000:
+        break # okay to quit once you see a swath of coords that are valid
+
+print(f'\nPart 1: {p1}')
 
 # Part 2
 mx = 20 if test else 4000000
 my = 20 if test else 4000000
 
+valid = False
 for s, b, dist in pairs:
-    print(s, b)
+    print(f'\rChecking {s}->{b}  ', end='')
     dist += 1
     sx, sy = s
     for d in range(0, dist):
@@ -80,20 +73,20 @@ for s, b, dist in pairs:
             y = sy + dy * (dist - d)
             if 0 <= x <= mx and 0 <= y <= my:
                 testb = (x, y)
-                #assert utils.manh(s, testb) == dist
                 valid = True
                 for s2, b2, dist2 in pairs:
-                    if s != s2:
-                        if utils.manh(s2, testb) <= dist2:
-                            valid = False
-                            break
-                if valid:
-                    break
+                    # check the Manhattan dist from this potential answer to every other sensor
+                    # if this dist is less than the sensor's distance to its beacon, it's not the place
+                    if s != s2 and utils.manh(s2, testb) <= dist2:
+                        valid = False
+                        break
             if valid:
                 break
+        if valid:
+            break
     if valid:
         break
 assert valid
 
 p2 = 4000000 * x + y
-print(f'Part 2: {p2}')
+print(f'\nPart 2: {p2}')
