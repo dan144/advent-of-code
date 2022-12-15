8f945a7402a195dd8987cacd1fc5a9e4446aeb96
Daniel Gross <dan.12sq@gmail.com>
2022-12-15 11:18:54 -0500

    2022 day 15 part 2

--- 2022/15.py
+++ 2022/15.py
@@ -29,7 +29,8 @@ specials = set()
 with open(input_file) as f:
     for line in f:
         sx, sy, bx, by = map(int, re.findall(r'-?\d+', line))
-        pairs.append(((sx, sy), (bx, by)))
+        d = utils.manh((sx, sy), (bx, by))
+        pairs.append(((sx, sy), (bx, by), d))
         grid[sy, sx] = 'S'
         grid[by, bx] = 'B'
         specials.add((sx, sy))
@@ -37,56 +38,62 @@ with open(input_file) as f:
 
 # Part 1
 
-for s, b in pairs:
+for s, b, d in pairs:
     sx, sy = s
     bx, by = b
-    d = utils.manh(s, b)
-    print(s, b, d)
+    #d = utils.manh(s, b)
     grid[sy + d, sx] = '.'
     grid[sy - d, sx] = '.'
     grid[sy, sx + d] = '.'
     grid[sy, sx - d] = '.'
-
-    #for x in range(-1 * d, d + 1):
-    #    for y in range(-1 * d, d + 1):
-    #        if abs(x) + abs(y) <= d:
-    #            c = sy + y, sx + x
-    #            if grid.get(c) is None:
-    #                #grid[c] = '#'
-    #                if sy + y == (10 if test else 2000000):
-    #                    p1 += 1
-    #                    print(p1)
-    #if test:
-    #    utils.display_grid(grid)
-    #    input()
-#min_x, min_y, max_x, max_y = utils.get_grid_edges(grid)
-#dx = max_x - min_x
-#y = 10 if test else 2000000
-#for x in range(min_x, max_x + 1):
-#    if grid.get((y, x)) == '#':
-#        p1 += 1
 
 min_x, min_y, max_x, max_y = utils.get_grid_edges(grid)
 print(min_x, min_y, max_x, max_y)
 if test:
     utils.display_grid(grid)
 
-y = 10 if test else 2000000
-for x in range(min_x, max_x + 1):
-    for s, b in pairs:
-        d = utils.manh(s, b)
-        if utils.manh(s, (x, y)) <= d and (x, y) not in specials:
-            p1 += 1
-            if test:
-                print(p1, (x, y))
-            else:
-                print(f'\r{p1}  ', end='')
-            break
-
-# not 5665305
-# not 5665304
-print(f'Part 1: {p1}')
+#y = 10 if test else 2000000
+#for x in range(min_x, max_x + 1):
+#    for s, b, d in pairs:
+#        #d = utils.manh(s, b)
+#        if utils.manh(s, (x, y)) <= d and (x, y) not in specials:
+#            p1 += 1
+#            if test:
+#                print(p1, (x, y))
+#            else:
+#                print(f'\r{p1}  ', end='')
+#            break
+#
+#print(f'Part 1: {p1}')
 
 # Part 2
+mx = 20 if test else 4000000
+my = 20 if test else 4000000
 
+for s, b, dist in pairs:
+    print(s, b)
+    dist += 1
+    sx, sy = s
+    for d in range(0, dist):
+        for dx, dy in utils.diags:
+            x = sx + dx * d
+            y = sy + dy * (dist - d)
+            if 0 <= x <= mx and 0 <= y <= my:
+                testb = (x, y)
+                #assert utils.manh(s, testb) == dist
+                valid = True
+                for s2, b2, dist2 in pairs:
+                    if s != s2:
+                        if utils.manh(s2, testb) <= dist2:
+                            valid = False
+                            break
+                if valid:
+                    break
+            if valid:
+                break
+    if valid:
+        break
+assert valid
+
+p2 = 4000000 * x + y
 print(f'Part 2: {p2}')
