52790652fb45399bbdff2b727fc123387d4b96e8
Daniel Gross <dan.12sq@gmail.com>
2022-12-21 16:46:34 -0500

    2022 day 21 part 2

--- 2022/21.py
+++ 2022/21.py
@@ -3,13 +3,15 @@
 import re
 import sys
 
+from copy import deepcopy
+
 test = len(sys.argv) > 1
 input_file = 'input' + sys.argv[0].split('.')[1].lstrip('/') + ('.test' if test else '')
 
 p1 = 0
 p2 = 0
 
-monkeys = {}
+o_monkeys = {}
 with open(input_file) as f:
     for line in f:
         monkey, equation = re.search(r'([a-z]+): (.*)', line.strip()).groups()
@@ -18,10 +20,11 @@ with open(input_file) as f:
         except Exception:
             pass
 
-        monkeys[monkey] = equation
+        o_monkeys[monkey] = equation
 
 # Part 1
 
+monkeys = deepcopy(o_monkeys)
 while str in map(lambda x: type(x), monkeys.values()):
     for monkey in monkeys.keys():
         if isinstance(monkeys[monkey], int):
@@ -39,4 +42,39 @@ print(f'Part 1: {p1}')
 
 # Part 2
 
+root = o_monkeys['root'].replace('+', '==')
+o_monkeys['humn'] = 'humn'
+while set(re.findall(r'([a-z]{4})', root)) != {'humn'}:
+    needs = re.findall(r'([a-z]{4})', root)
+    for n in needs:
+        if isinstance(o_monkeys[n], int) or o_monkeys[n] == 'humn':
+            root = root.replace(n, f'{o_monkeys[n]}')
+        else:
+            root = root.replace(n, f'({o_monkeys[n]})')
+root = root.replace('humn', 'x').replace(' ', '')
+while n := re.search(r'\(\d+[-+\*/]\d+\)', root):
+    if n is None:
+        break
+    n = n.group(0)
+    root = root.replace(n, str(int(eval(n))))
+
+while re.fullmatch(r'x==\d+', root) is None:
+    if g := re.fullmatch(r'\((\d+)\+(.*)\)==(\d+)', root):
+        root = f'{g.groups()[1]}=={int(g.groups()[2]) - int(g.groups()[0])}'
+    elif g := re.fullmatch(r'\((\d+)-(.*)\)==(\d+)', root):
+        root = f'{g.groups()[1]}=={int(g.groups()[0]) - int(g.groups()[2])}'
+    elif g := re.fullmatch(r'\((\d+)\*(.*)\)==(\d+)', root):
+        root = f'{g.groups()[1]}=={int(g.groups()[2]) // int(g.groups()[0])}'
+    elif g := re.fullmatch(r'\((\d+)/(.*)\)==(\d+)', root):
+        root = f'{g.groups()[1]}=={int(g.groups()[0]) // int(g.groups()[2])}'
+    elif g := re.fullmatch(r'\((.*)\+(\d+)\)==(\d+)', root):
+        root = f'{g.groups()[0]}=={int(g.groups()[2]) - int(g.groups()[1])}'
+    elif g := re.fullmatch(r'\((.*)-(\d+)\)==(\d+)', root):
+        root = f'{g.groups()[0]}=={int(g.groups()[2]) + int(g.groups()[1])}'
+    elif g := re.fullmatch(r'\((.*)\*(\d+)\)==(\d+)', root):
+        root = f'{g.groups()[0]}=={int(g.groups()[2]) // int(g.groups()[1])}'
+    elif g := re.fullmatch(r'\((.*)/(\d+)\)==(\d+)', root):
+        root = f'{g.groups()[0]}=={int(g.groups()[1]) * int(g.groups()[2])}'
+
+p2 = root.split('==')[1]
 print(f'Part 2: {p2}')
