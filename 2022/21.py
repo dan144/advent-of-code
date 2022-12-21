d093497740eb9fc5c37554954ebbd6342dea3af1
Daniel Gross <dan.12sq@gmail.com>
2022-12-21 17:09:44 -0500

    2022 day 21 cleanup

--- 2022/21.py
+++ 2022/21.py
@@ -8,9 +8,6 @@ from copy import deepcopy
 test = len(sys.argv) > 1
 input_file = 'input' + sys.argv[0].split('.')[1].lstrip('/') + ('.test' if test else '')
 
-p1 = 0
-p2 = 0
-
 o_monkeys = {}
 with open(input_file) as f:
     for line in f:
@@ -48,17 +45,16 @@ while set(re.findall(r'([a-z]{4})', root)) != {'humn'}:
     needs = re.findall(r'([a-z]{4})', root)
     for n in needs:
         if isinstance(o_monkeys[n], int) or o_monkeys[n] == 'humn':
-            root = root.replace(n, f'{o_monkeys[n]}')
+            root = root.replace(n, str(o_monkeys[n]))
         else:
             root = root.replace(n, f'({o_monkeys[n]})')
-root = root.replace('humn', 'x').replace(' ', '')
+
+root = root.replace(' ', '')
 while n := re.search(r'\(\d+[-+\*/]\d+\)', root):
-    if n is None:
-        break
     n = n.group(0)
     root = root.replace(n, str(int(eval(n))))
 
-while re.fullmatch(r'x==\d+', root) is None:
+while re.fullmatch(r'humn==\d+', root) is None:
     if g := re.fullmatch(r'\((\d+)\+(.*)\)==(\d+)', root):
         root = f'{g.groups()[1]}=={int(g.groups()[2]) - int(g.groups()[0])}'
     elif g := re.fullmatch(r'\((\d+)-(.*)\)==(\d+)', root):
